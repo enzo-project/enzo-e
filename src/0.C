c200734f9934aa01529e2a9fc41a23b91e88c238
bordner <none@none>
2007-07-12 22:54:47 +0000

    Added 0.C template and Doxyfile

---
+++ src/0.C
@@ -0,0 +1,32 @@
+/ $Id$
+/**
+ * @file
+ * @brief Write description of source file
+ * @brief here for dOxygen.
+ * @author Me
+ * @version 1.69
+ */
+// $Log$
+ 
+///  Single line comment for dOxygen.
+ 
+/**
+   Write description of function here.
+   The function should follow these
+   comments.
+  
+   The function arguments listed with "param" will be compared 
+   to the declaration and verified.
+
+   @param argOne Description of first function argument.
+   @param argTwo Description of second function argument.
+   @return Description of returned value.
+ */
+ 
+int
+class::function(arg1, arg2)
+{
+ 
+    /// Single line comment for dOxygen.
+}
+    
