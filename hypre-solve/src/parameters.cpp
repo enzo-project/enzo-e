49546318abed5fa687159fbcc8b0c06ff148ae21
bordner <none@none>
2007-08-31 02:28:11 +0000

    Added Parameters class; moved boundary condition selection from code to "boundary" input variable

---
+++ hypre-solve/src/parameters.cpp
@@ -0,0 +1,185 @@
+
+/// Parameters class for representing a set of parameters read from an input file.
+
+/**
+ * 
+ * 
+ * 
+ * @file
+ * @author James Bordner <jobordner@ucsd.edu>
+ *
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <mpi.h>
+
+#include <string>
+#include <map>
+
+#include "hypre-solve.hpp"
+#include "parameters.hpp"
+
+const int trace = 0;
+
+//----------------------------------------------------------------------
+
+/// Create a Parameters object
+
+/** Creates an empty Parameters object. */
+
+Parameters::Parameters () throw ()
+{
+  _TRACE_;
+}
+
+//----------------------------------------------------------------------
+
+/// Delete a Parameters object
+
+/** Deletes an empty Parameters object. */
+
+Parameters::~Parameters () throw ()
+{
+  _TRACE_;
+}
+
+//----------------------------------------------------------------------
+
+/// Read a file of key-value pairs.
+
+/** Reads a file of key-value pairs.  Allows more than one value per key. */
+
+void Parameters::read (std::string filename) throw ()
+{
+
+  _TRACE_;
+
+  // Open the file and initialize the line buffer
+
+  FILE *fp = fopen (filename.c_str(),"r");
+  char buffer[BUFFER_LENGTH];
+  char key[BUFFER_LENGTH];
+  int i;
+
+  // Read the file line by line
+
+  while (readline_ (fp,buffer,BUFFER_LENGTH)) {
+
+    // get keys in key[]
+    for (i=0; i<BUFFER_LENGTH; i++) key[i]=0;
+    sscanf(buffer,"%s",key);
+    const char * values = buffer + strlen(key);
+
+    // Skip over initial white space
+    while (isspace(*++values))
+      ;
+
+    if (strcmp(key,"include") == 0) {
+
+      printf ("%s:%d Including %s\n",__FILE__,__LINE__,values);
+      // Recursively read included input files.  No error checking
+
+      this->read(values);
+
+    } else {
+
+      add_parameter(key,values);
+
+    }
+
+  }
+
+}
+
+//----------------------------------------------------------------------
+
+/// Print all parameters to stdout.
+
+/** Print all parameters to stdout. */
+
+void Parameters::print () throw ()
+{
+  printf ("Parameters\n");
+  
+  for( std::multimap<std::string, std::string>::iterator iter = values_.begin(); 
+       iter != values_.end(); 
+       ++iter ) {
+    printf ("     Key: %s\n",iter->first.c_str());
+    printf ("   Value: %s\n",iter->second.c_str());
+  }
+}
+//----------------------------------------------------------------------
+
+/// Associate the given value with the given key.
+
+/** Associate the given value with the given key. */
+
+void Parameters::add_parameter (std::string key, std::string val) throw ()
+{
+  _TRACE_;
+  // Do nothing if key is empty or a comment ("#" or "//")
+  if (key=="" || key=="#" || key=="//") return;
+  values_.insert( std::pair<std::string,std::string>(key,val) );
+  printf ("DEBUG add_parameter (%s,%s)\n",key.c_str(),val.c_str());
+}
+
+//----------------------------------------------------------------------
+
+/// Retrieve the ith value of the the given parameter.
+
+/** Retrieve the ith value of the the given parameter. */
+
+std::string Parameters::ith_value  (std::string key, int i) const throw ()
+{
+  _TRACE_;
+  return "";
+}
+
+//----------------------------------------------------------------------
+
+/// Return the multiplicity of values for the given key.
+
+/**  Return the multiplicity of values for the given key.  May be 0. */
+
+int Parameters::num_values  (std::string key) const throw ()
+{
+  _TRACE_;
+  return 0;
+}
+
+//----------------------------------------------------------------------
+
+/// Return the value for the given key.
+
+/**  Return the first value for the given key.  Returns "" if not defined. */
+
+std::string Parameters::value (std::string key) const throw ()
+{
+  return (values_.find(key) == values_.end()) ? "" : values_.find(key)->second;
+}
+
+//======================================================================
+
+int Parameters::readline_ (FILE* fp, char * buffer, int n) throw()
+{
+  _TRACE_;
+  int i;
+  // Clear the line buffer
+  for (i=0; i<BUFFER_LENGTH; i++) buffer[i]=0;
+  i=0;
+  int c;
+  buffer[i] = c = fgetc(fp);
+  while (c != EOF && c != '\n' && i < n-1) {
+    ++i;
+    buffer[i] = c = fgetc(fp);
+  }
+  if (buffer[i] == '\n') buffer[i] = '\0';
+  if (i == n-1) {
+    fprintf (stderr,"Line too long: %s\n",buffer);
+    exit(1);
+  }
+  _TRACE_;
+  return (c != EOF);
+}
+
---
+++ hypre-solve/src/parameters.hpp
@@ -0,0 +1,113 @@
+
+/// Parameters class include file
+
+/**
+ * The Parameters class is used for storing key-value pairs of strings, where
+ * the value may not necessarily be unique.
+ * 
+ * @file
+ * @author James Bordner <jobordner@ucsd.edu>
+ * @date 2007-08-30 
+ *
+ */
+
+#define BUFFER_LENGTH 255
+
+class Parameters 
+{
+
+  friend class ItParameters;
+
+private:
+
+  std::multimap<std::string, std::string> values_;
+
+public:
+
+  Parameters () throw () ;
+  ~Parameters () throw () ;
+
+  /// Read a file of key-value pairs.  May be more than one value per key.
+  void read (std::string file) throw () ;
+
+  /// Print all parameters to stdout.
+  void print () throw () ;
+
+  /// Associate the given value with the given key.
+  void add_parameter (std::string key, std::string val) throw () ;
+
+  /// Retrieve the ith value of the the given parameter.
+  std::string ith_value  (std::string key, int i) const throw () ;
+
+  /// Return the multiplicity of values for the given key.  May be 0.
+  int num_values  (std::string key) const throw () ;
+
+  /// Return the value for the given key.
+  std::string value (std::string key) const throw ();
+
+private:
+
+  int readline_ (FILE *, char * buffer, int n) throw ();
+
+};
+
+/// ItParameters class
+
+/**
+ * 
+ * An ItParameters object allows iterating through all key-value pairs in
+ * a Parameters object.
+ * 
+ * @file
+ * @author James Bordner <jobordner@ucsd.edu>
+ * @date 2007-08-30
+ *
+ */
+
+class ItParameters
+{
+
+  //--------------------------------------------------------------------
+  // PRIVATE MEMBER DATA
+  //--------------------------------------------------------------------
+
+private:
+
+  std::multimap<std::string, std::string>::iterator curr_;
+  std::multimap<std::string, std::string>::iterator next_;
+  Parameters * pparameters_;
+
+public:
+
+  //--------------------------------------------------------------------
+  // CONSTUCTORS AND DESTRUCTORS
+  //--------------------------------------------------------------------
+
+  ItParameters (Parameters & parameters) throw ()
+    : pparameters_(& parameters),
+      next_(parameters.values_.begin()),
+      curr_()
+  { }
+
+  ~ItParameters () throw () {};
+  
+  /// Iterate through all Grids in the Grid.
+  int operator++ (int) { 
+    
+    curr_ = next_;
+    int not_done = 1;
+    if (next_ == pparameters_->values_.end()) {
+      next_ = pparameters_->values_.begin();
+      not_done = 0;
+    }
+    ++next_;
+    return not_done;
+  }
+
+  std::string key () throw ()
+  { return curr_->first; }
+  std::string value () throw ()
+  { return curr_->second; }
+
+};
+
