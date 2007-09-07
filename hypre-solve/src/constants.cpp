d4c7f7d6959b7cc34690e7e5d2d233b27e597bdd
bordner <none@none>
2007-09-07 18:44:54 +0000

    Added constants.cpp; updated makefile

--- hypre-solve/src/constants.cpp
+++ hypre-solve/src/constants.cpp
@@ -1,2 +1,5 @@
-static Constants constants;
+#include "scalar.hpp"
+#include "constants.hpp"
 
+const Scalar Constants::pi_ = 3.14159265358979323;
+const Scalar Constants::G_  = 6.67428e-8;
--- hypre-solve/src/constants.hpp
+++ hypre-solve/src/constants.hpp
@@ -16,8 +16,8 @@ class Constants
 
 private:
 
-  static const Scalar pi_ = 3.14159265358979323;
-  static const Scalar G_  = 6.67428e-8;          // CGS +/- 0.0007e-8
+  static const Scalar pi_;
+  static const Scalar G_;          // CGS +/- 0.0007e-8
 
 public:
 
