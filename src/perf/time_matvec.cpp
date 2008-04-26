9d491b015d2dd04a724a0ee48b3e1921162d90e2
bordner <none@none>
2008-04-26 01:54:38 +0000

    Updated time_matvec.cpp

--- src/perf/time_matvec.cpp
+++ src/perf/time_matvec.cpp
@@ -1,13 +1,13 @@
 /** 
- *********************************************************************
- *
- * @file      time_matvec.cpp
- * @brief     Program timing sparse matrix-vector multiplies
- * @author    James Bordner
- * @date      Thu Apr 24 16:25:43 PDT 2008
- *
- *********************************************************************
- */
+*********************************************************************
+*
+* @file      time_matvec.cpp
+* @brief     Program timing sparse matrix-vector multiplies
+* @author    James Bordner
+* @date      Thu Apr 24 16:25:43 PDT 2008
+*
+*********************************************************************
+*/
  
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,52 +20,158 @@
 
 main()
 {
-  Timer timer;
-  int k,j,i0,i1,i2;
+  Timer timer_const;
+  Timer overhead;
+  Timer timer_symm;
+  Timer timer_full;
+  int i,j,k,i0,i1,i2;
+
+  const int CACHE_SIZE = 4096;
+  char cache[CACHE_SIZE];
 
   for (k=8; k<=192; k++) {
 
-    int k2 = k + 2;
-    Array A0(k2,k2,k2);  Scalar  * a0 = A0.values();
-    Array AXP(k2,k2,k2); Scalar * axp = AXP.values();
-    Array AXM(k2,k2,k2); Scalar * axm = AXM.values();
-    Array AYP(k2,k2,k2); Scalar * ayp = AYP.values();
-    Array AYM(k2,k2,k2); Scalar * aym = AYM.values();
-    Array AZP(k2,k2,k2); Scalar * azp = AZP.values();
-    Array AZM(k2,k2,k2); Scalar * azm = AZM.values();
-    Array   X(k2,k2,k2); Scalar   * x =   X.values();
-    Array   B(k2,k2,k2); Scalar    *b =   B.values();
-
-    if (a0 && axp && axm && ayp && aym && azp && azm) {
-    timer.start();
+    int k2  = k + 2;
+    int k22 = k2*k2;
 
     int count = 1000000/k/k/k+1;
-    for (j=0; j<count; j++) {
-    for (i0=1; i0<k+1; i0++) {
-      for (i1=1; i1<k+1; i1++) {
-	for (i2=1; i2<k+1; i2++) {
-	  int i = i0 + k2*(i1 + k2*i2);
-	  b[i] = azm[i-k2*k2]*x[i-k2*k2]
-	    +    aym[i-k2]   *x[i-k2]
-	    +    axm[i-1]    *x[i-1]
-	    +     a0[i]      *x[i]
-	    +    axm[i+1]    *x[i+1]
-	    +    aym[i+k2]   *x[i+k2]
-	    +    azm[i+k2*k2]*x[i+k2*k2];
+    {
+      Array A0(k2,k2,k2);  Scalar  * a0 = A0.values();
+      Array AXP(k2,k2,k2); Scalar * axp = AXP.values();
+      Array AXM(k2,k2,k2); Scalar * axm = AXM.values();
+      Array AYP(k2,k2,k2); Scalar * ayp = AYP.values();
+      Array AYM(k2,k2,k2); Scalar * aym = AYM.values();
+      Array AZP(k2,k2,k2); Scalar * azp = AZP.values();
+      Array AZM(k2,k2,k2); Scalar * azm = AZM.values();
+      Array   X(k2,k2,k2); Scalar   * x =   X.values();
+      Array   B(k2,k2,k2); Scalar    *b =   B.values();
+
+      if (a0 && axp && axm && ayp && aym && azp && azm) {
+
+	for (j=0; j<count; j++) {
+
+	  timer_const.start();
+
+	  for (i0=1; i0<k+1; i0++) {
+	    for (i1=1; i1<k+1; i1++) {
+	      for (i2=1; i2<k+1; i2++) {
+		i = i0 + k2*(i1 + k2*i2);
+		b[i] = azm[0]*x[i-k22]
+		  +    aym[0]*x[i-k2]
+		  +    axm[0]*x[i-1]
+		  +     a0[0]*x[i]
+		  +    axp[0]*x[i+1]
+		  +    ayp[0]*x[i+k2]
+		  +    azp[0]*x[i+k22];
+	      }
+	    }
+	  }
+	  timer_const.stop();
+
+	  for (i=0; i<CACHE_SIZE; i++) {
+	    cache[i] = 1;
+	  }
+	}
+
+	for (j=0; j<count; j++) {
+	  overhead.start();
+	  overhead.stop();
+	  for (i=0; i<CACHE_SIZE; i++) {
+	    cache[i] = 1;
+	  }
 	}
       }
     }
+
+    {
+      Array A0(k2,k2,k2);  Scalar  * a0 = A0.values();
+      Array AXP(k2,k2,k2); Scalar * axp = AXP.values();
+      Array AXM(k2,k2,k2); Scalar * axm = AXM.values();
+      Array AYP(k2,k2,k2); Scalar * ayp = AYP.values();
+      Array AYM(k2,k2,k2); Scalar * aym = AYM.values();
+      Array AZP(k2,k2,k2); Scalar * azp = AZP.values();
+      Array AZM(k2,k2,k2); Scalar * azm = AZM.values();
+      Array   X(k2,k2,k2); Scalar   * x =   X.values();
+      Array   B(k2,k2,k2); Scalar    *b =   B.values();
+    
+      if (a0 && axp && axm && ayp && aym && azp && azm) {
+
+
+	for (j=0; j<count; j++) {
+	  timer_symm.start();
+	  for (i0=1; i0<k+1; i0++) {
+	    for (i1=1; i1<k+1; i1++) {
+	      for (i2=1; i2<k+1; i2++) {
+		i = i0 + k2*(i1 + k2*i2);
+		b[i] = azp[i]*x[i-k22]
+		  +    ayp[i]   *x[i-k2]
+		  +    axp[i]    *x[i-1]
+		  +     a0[i]      *x[i]
+		  +    axp[i+1]    *x[i+1]
+		  +    ayp[i+k2]   *x[i+k2]
+		  +    azp[i+k22]*x[i+k22];
+	      }
+	    }
+	  }
+	  timer_symm.stop();
+	  for (i=0; i<CACHE_SIZE; i++) {
+	    cache[i] = 1;
+	  }
+	}
+      }
     }
 
-    timer.stop();
+    {
+      Array A0(k2,k2,k2);  Scalar  * a0 = A0.values();
+      Array AXP(k2,k2,k2); Scalar * axp = AXP.values();
+      Array AXM(k2,k2,k2); Scalar * axm = AXM.values();
+      Array AYP(k2,k2,k2); Scalar * ayp = AYP.values();
+      Array AYM(k2,k2,k2); Scalar * aym = AYM.values();
+      Array AZP(k2,k2,k2); Scalar * azp = AZP.values();
+      Array AZM(k2,k2,k2); Scalar * azm = AZM.values();
+      Array   X(k2,k2,k2); Scalar   * x =   X.values();
+      Array   B(k2,k2,k2); Scalar    *b =   B.values();
+    
+      if (a0 && axp && axm && ayp && aym && azp && azm) {
+
+	for (j=0; j<count; j++) {
+	timer_full.start();
+	  for (i0=1; i0<k+1; i0++) {
+	    for (i1=1; i1<k+1; i1++) {
+	      for (i2=1; i2<k+1; i2++) {
+		i = i0 + k2*(i1 + k2*i2);
+		b[i] = azm[i-k22]*x[i-k22]
+		  +    aym[i-k2]   *x[i-k2]
+		  +    axm[i-1]    *x[i-1]
+		  +     a0[i]      *x[i]
+		  +    axp[i+1]    *x[i+1]
+		  +    ayp[i+k2]   *x[i+k2]
+		  +    azp[i+k22]*x[i+k22];
+	      }
+	    }
+	  }
+	  timer_full.stop();
+	  for (i=0; i<CACHE_SIZE; i++) {
+	    cache[i] = 1;
+	  }
+	}
+      }
+    }
+
+
     int num_flops = count*k*k*k*13;
-    printf ("%d %20.12f %20.12f \n",k,timer.value()/count, 1e-6*num_flops/timer.value()); 
+    printf ("%d %20.12f %20.12f %20.12f \n",
+	    k,
+	    1e-6*num_flops/(timer_const.value()-overhead.value()),
+	    1e-6*num_flops/(timer_symm.value()-overhead.value()),
+	    1e-6*num_flops/(timer_full.value()-overhead.value())
+	    ); 
     fflush(stdout);
-    timer.clear();
-    } else {
-    printf ("%d 0\n",k); 
-    fflush(stdout);
-    }
 
-  }
+    timer_const.clear();
+    timer_symm.clear();
+    timer_full.clear();
+    overhead.clear();
+
+  } // for
 }
