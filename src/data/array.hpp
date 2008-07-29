e81d95a2085902de49b00be4c7a275f1c969baf4
bordner <none@none>
2008-07-29 01:25:21 +0000

    adding copyright

--- src/data/array.hpp
+++ src/data/array.hpp
@@ -1,3 +1,16 @@
+/*
+ * ENZO: THE NEXT GENERATION
+ *
+ * A parallel astrophysics and cosmology application
+ *
+ * Copyright (C) 2008 James Bordner
+ * Copyright (C) 2008 Laboratory for Computational Astrophysics
+ * Copyright (C) 2008 Regents of the University of California
+ *
+ * See CELLO_LICENSE in the main directory for full license agreement
+ *
+ */
+
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
