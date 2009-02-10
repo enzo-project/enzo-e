bdaa46f22b360e15ab9e97557412cebe6ab8b7cc
bordner <none@none>
2009-02-10 02:12:23 +0000

    cleaning and fixing error.hpp

--- hypre-solve/src/error.hpp
+++ hypre-solve/src/error.hpp
@@ -17,7 +17,7 @@
    int _ip_; \
    MPI_Comm_rank (MPI_COMM_WORLD, &_ip_); \
    printf ("WARNING %d %s:%d %s\n",_ip_,__FILE__,__LINE__,MESSAGE);	\
-  fflush(stdout); \
+   fflush(stdout); \
 }
 
 #define ERROR(MESSAGE) { \
@@ -25,6 +25,7 @@
    MPI_Comm_rank (MPI_COMM_WORLD, &_ip_); \
    printf ("ERROR %d %s:%d %s\n",_ip_,__FILE__,__LINE__,MESSAGE); \
   fflush(stdout); \
+  exit(1); \
 }
 
 #define WARNING_ROOT(X) { \
@@ -33,7 +34,8 @@
    if (_ip_==0) { \
       printf ("WARNING %d %s:%d" X "\n",__FILE__,__LINE__); \
       fflush(stdout);\
-}
+   } \
+  }
 
 #define NOT_IMPLEMENTED(X) { \
    printf ("%s:%d WARNING: " X " is not implemented yet\n",__FILE__,__LINE__); \
