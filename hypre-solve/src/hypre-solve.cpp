74568029b64c496d1ea4eab1c67dbcf488d02600
bordner <none@none>
2009-06-23 20:27:24 +0000

    Removing trace, jbperf, and barriers

--- hypre-solve/src/hypre-solve.cpp
+++ hypre-solve/src/hypre-solve.cpp
@@ -81,11 +81,6 @@ int main(int argc, char **argv)
   // --------------------------------------------------
 
 
-  // ***************
-  JBPERF_BEGIN("HS");
-  JBPERF_START("0-total");
-  // ***************
-
   if (argc==2) {
 
     std::string filename (argv[1]);
@@ -93,148 +88,71 @@ int main(int argc, char **argv)
     // --------------------------------------------------
     // Problem initialization
     // --------------------------------------------------
-    _TRACE_;
 
     // create a new problem and read it in
 
     Problem problem;
 
-    // ***************
-    JBPERF_START("1-problem");
-    // ***************
-
     problem.read  (filename);
-
-    // ***************
-    JBPERF_STOP("1-problem");
-    // ***************
 
     // --------------------------------------------------
     // Initialize the hierarchy
     // --------------------------------------------------
-    _TRACE_;
 
     Hierarchy & hierarchy = problem.hierarchy();
-    Grid::set_domain (problem.domain());
-    Level::set_domain (problem.domain());
+    Grid::set_domain (problem.domain());  // only needed by geomview viz
+    Level::set_domain (problem.domain()); // only needed by geomview viz
 
     // determine interconnections between grids
 
-    // ***************
-    JBPERF_START("2-grids");
-    // ***************
-
     bool is_periodic = problem.parameters().value("boundary") == "periodic";
     hierarchy.initialize(problem.domain(), *pmpi,is_periodic);
-
-    // ***************
-    JBPERF_STOP("2-grids");
-    // ***************
 
     if (debug) problem.print ();
 
     // WARNING: problem-size returns the size of the root-grid, not the entire hierarchy
 
-    JBPERF_GLOBAL("problem-size",problem.hierarchy().num_unknowns0());
-    JBPERF_GLOBAL("problem-levels",problem.hierarchy().num_levels());
-
     // --------------------------------------------------
     // Initialize hypre
     // --------------------------------------------------
-    _TRACE_;
 
     Hypre hypre (hierarchy,problem.parameters());
 
-    // ***************
-    JBPERF_START("4-hypre-init");
-    // ***************
 
-    if (barrier) pmpi->barrier();
     hypre.init_hierarchy (*pmpi);
 
     // Initialize the stencils
     
-    if (barrier) pmpi->barrier();
     hypre.init_stencil ();
 
     // Initialize the graph
 
-    if (barrier) pmpi->barrier();
     hypre.init_graph ();
 
     // Initialize the elements of matrix A and vector B
 
-    if (barrier) pmpi->barrier();
     hypre.init_elements (problem.points());
-
-    // ***************
-    JBPERF_STOP("4-hypre-init");
-    // ***************
 
     // --------------------------------------------------
     // Solve the linear system A X = B
     // --------------------------------------------------
-    _TRACE_;
 
-    // ***************
-    JBPERF_START("5-hypre-solve");
-    // ***************
-
-    if (barrier) pmpi->barrier();
     hypre.solve ();
-
-    // ***************
-    JBPERF_STOP("5-hypre-solve");
-    // ***************
 
     // --------------------------------------------------
     // Evaluate the solution
     // --------------------------------------------------
-    _TRACE_;
 
-    if (barrier) pmpi->barrier();
     hypre.evaluate ();
 
     // --------------------------------------------------
     // jbPerf Finalize
     // --------------------------------------------------
-    _TRACE_;
-
-    // ****************
-    JBPERF_STOP("0-total");
-    JBPERF_END("HS");
-    // ***************
 
     // --------------------------------------------------
     // MPI Finalize
     // --------------------------------------------------
-    _TRACE_;
 
-    //    pmpi->barrier();
-
-    if (barrier) pmpi->barrier();
-
-    if (pmpi->is_root()) { 
-      bool success = true;
-      // Residual too high
-      int itmax     = atoi(problem.parameters().value("solver_itmax").c_str());
-      double restol = atof(problem.parameters().value("solver_restol").c_str());
-      if (hypre.residual() > restol) {
-	printf ("Diverged %s: %g > %g\n",exec_name.c_str(),
-		hypre.residual(),restol); fflush(stdout); 
-	success = false;
-      }
-      // Iterations reached limit
-      if (hypre.iterations() >= itmax) {
-	printf ("Stalled %s: %d >= %d\n",exec_name.c_str(),
-		hypre.iterations(),itmax); fflush(stdout); 
-	success = false;
-      }
-      // Appears to have completed successfully
-      if (success) {
-	printf ("Success %s\n",exec_name.c_str()); fflush(stdout); 
-      }
-    }
 
     MPI_Finalize();
     delete pmpi;
