661d740b239f1c127a056aab78383899b7302d40
bordner <none@none>
2009-07-08 22:56:08 +0000

    Adding performance.hpp (wasn't there before?)

---
+++ src/Performance/performance.hpp
@@ -0,0 +1,29 @@
+#ifndef PERFORMANCE_HPP
+#define PERFORMANCE_HPP
+
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+/*
+ * ENZO: THE NEXT GENERATION
+ *
+ * A parallel astrophysics and cosmology application
+ *
+ * Copyright (C) 2008 James Bordner
+ * Copyright (C) 2008 Laboratory for Computational Astrophysics
+ * Copyright (C) 2008 Regents of the University of California
+ *
+ * See CELLO_LICENSE in the main directory for full license agreement
+ *
+ */
+
+
+#include <sys/time.h>
+#ifdef __linux__
+#include <unistd.h>
+#endif
+
+#include "cello.def"
+
+#include "performance_timer_.hpp"
+
+#endif /* PERFORMANCE_HPP */
