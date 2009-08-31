db16681cafb292277240a2dc373af47fa0e43b14
bordner <none@none>
2009-08-31 21:44:53 +0000

    Cello: Updating template copyrights to 2009

--- src/TEMPLATE/TEMPLATE.cpp
+++ src/TEMPLATE/TEMPLATE.cpp
@@ -5,9 +5,9 @@
  *
  * A parallel astrophysics and cosmology application
  *
- * Copyright (C) 2008 James Bordner
- * Copyright (C) 2008 Laboratory for Computational Astrophysics
- * Copyright (C) 2008 Regents of the University of California
+ * Copyright (C) 2009 James Bordner
+ * Copyright (C) 2009 Laboratory for Computational Astrophysics
+ * Copyright (C) 2009 Regents of the University of California
  *
  * See CELLO_LICENSE in the main directory for full license agreement
  *
@@ -20,9 +20,9 @@
  * @file      TEMPLATE.cpp
  * @brief     Brief description of the file
  * @author    Bart Simpson
- * @date      Thu Feb 21 12:45:56 PST 2008
+ * @date      Thu Feb 21 12:45:56 PST 2009
  * @bug       Example bug description 
- * @note      Modified by Alfred E. Newman, Thu Feb 21 12:49:16 PST 2008
+ * @note      Modified by Alfred E. Newman, Thu Feb 21 12:49:16 PST 2009
  *            description of modification
  *
  * DESCRIPTION 
