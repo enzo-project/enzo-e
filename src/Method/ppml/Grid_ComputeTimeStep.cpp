e0344c35796c1a43a9fd7ffa3b1fed9195fb9208
bordner <none@none>
2009-10-05 18:13:31 +0000

    updating ppml

---
+++ src/Method/ppml/Grid_ComputeTimeStep.cpp
@@ -0,0 +1,221 @@
+/***********************************************************************
+/
+/  GRID CLASS (COMPUTE TIME STEP)
+/
+/  written by: Greg Bryan
+/  date:       November, 1994
+/  modified1: Alexei Kritsuk, May 2009 - added PPML/MHD timestep restriction
+/
+/  PURPOSE:
+/
+/  RETURNS:
+/    dt   - timestep
+/
+************************************************************************/
+ 
+// Compute the timestep from all the constrains for this grid.
+//
+// Somebody fix the error handling in this routine! please.
+//
+ 
+#include "cello_hydro.h"
+ 
+/* function prototypes */
+ 
+void my_exit(int status);
+
+int CosmologyComputeExpansionTimestep(FLOAT time, float *dtExpansion);
+int CosmologyComputeExpansionFactor(FLOAT time, FLOAT *a, FLOAT *dadt);
+extern "C" void FORTRAN_NAME(calc_dt)(
+                  int *rank, int *idim, int *jdim, int *kdim,
+                  int *i1, int *i2, int *j1, int *j2, int *k1, int *k2,
+			     int *ihydro, float *C2,
+                  FLOAT *dx, FLOAT *dy, FLOAT *dz, float *vgx, float *vgy,
+                             float *vgz, float *gamma, int *ipfree, float *aye,
+                  float *d, float *p, float *u, float *v, float *w,
+			     float *dt, float *dtviscous);
+ 
+extern "C" void FORTRAN_NAME(calc_dt_ppml)(
+                  int *idim, int *jdim, int *kdim,
+                  int *i1, int *i2, int *j1, int *j2, int *k1, int *k2,
+                  FLOAT *dx, FLOAT *dy, FLOAT *dz,
+                  float *dn, float *vx, float *vy, float *vz, 
+                             float *bx, float *by, float *bz, 
+			     float *dt);
+ 
+ 
+float ComputeTimeStep()
+{
+ 
+  /* initialize */
+ 
+  float dt, dtTemp;
+  float dtBaryons      = HUGE_VAL;
+  float dtViscous      = HUGE_VAL;
+  float dtParticles    = HUGE_VAL;
+  float dtExpansion    = HUGE_VAL;
+  float dtAcceleration = HUGE_VAL;
+  int dim, i, result;
+ 
+  /* Compute the field size. */
+ 
+  int size = 1;
+  for (dim = 0; dim < GridRank; dim++)
+    size *= GridDimension[dim];
+ 
+  /* If using comoving coordinates, compute the expansion factor a.  Otherwise,
+     set it to one. */
+ 
+  FLOAT a = 1, dadt;
+  if (ComovingCoordinates)
+    CosmologyComputeExpansionFactor(Time, &a, &dadt);
+  float afloat = float(a);
+ 
+  /* 1) Compute Courant condition for baryons. */
+ 
+  if (NumberOfBaryonFields > 0) {
+ 
+    /* Find fields: density, total energy, velocity1-3. */
+ 
+    int DensNum, GENum, Vel1Num, Vel2Num, Vel3Num, TENum;
+    float *pressure_field;
+
+//     if (HydroMethod != PPML_Isothermal3D) {
+//       if (this->IdentifyPhysicalQuantities(DensNum, GENum, Vel1Num, Vel2Num,
+// 					 Vel3Num, TENum) == FAIL) {
+// 	fprintf(stderr, "ComputeTimeStep: IdentifyPhysicalQuantities error.\n");
+// 	exit(FAIL);
+//       }
+ 
+//     /* Compute the pressure. */
+ 
+//       pressure_field = new float[size];
+//       if (DualEnergyFormalism)
+// 	result = this->ComputePressureDualEnergyFormalism(Time, pressure_field);
+//       else
+// 	result = this->ComputePressure(Time, pressure_field);
+ 
+//       if (result == FAIL) {
+// 	fprintf(stderr, "Error in grid->ComputePressure.\n");
+// 	exit(EXIT_FAILURE);
+//       }
+//     }
+
+#ifdef UNUSED
+    int Zero[3] = {0,0,0}, TempInt[3] = {0,0,0};
+    for (dim = 0; dim < GridRank; dim++)
+      TempInt[dim] = GridDimension[dim]-1;
+#endif /* UNUSED */
+ 
+    /* Call fortran routine to do calculation. */
+ 
+//     if (HydroMethod == PPML_Isothermal3D) {
+//       if (GridRank !=3) 
+// 	my_exit(EXIT_FAILURE);
+      FORTRAN_NAME(calc_dt_ppml)
+	(GridDimension, GridDimension+1, GridDimension+2,
+	 GridStartIndex, GridEndIndex,
+	 GridStartIndex+1, GridEndIndex+1,
+	 GridStartIndex+2, GridEndIndex+2,
+	 CellWidth[0], CellWidth[1], CellWidth[2],
+	 BaryonField[0], 
+	 BaryonField[1], BaryonField[2], BaryonField[3], 
+	 BaryonField[4], BaryonField[5], BaryonField[6], 
+	 &dtBaryons);
+
+      //     }
+ 
+//     else
+//       FORTRAN_NAME(calc_dt)(&GridRank, GridDimension, GridDimension+1,
+//                                GridDimension+2,
+// //                        Zero, TempInt, Zero+1, TempInt+1, Zero+2, TempInt+2,
+//                           GridStartIndex, GridEndIndex,
+//                                GridStartIndex+1, GridEndIndex+1,
+//                                GridStartIndex+2, GridEndIndex+2,
+// 			       &HydroMethod, &ZEUSQuadraticArtificialViscosity,
+//                           CellWidth[0], CellWidth[1], CellWidth[2],
+//                                GridVelocity, GridVelocity+1, GridVelocity+2,
+//                                &Gamma, &PressureFree, &afloat,
+//                           BaryonField[DensNum], pressure_field,
+//                                BaryonField[Vel1Num], BaryonField[Vel2Num],
+//                                BaryonField[Vel3Num], &dtBaryons, &dtViscous);
+ 
+    /* Clean up */
+ 
+//     if (HydroMethod != PPML_Isothermal3D)
+//       delete pressure_field;
+ 
+    /* Multiply resulting dt by CourantSafetyNumber (for extra safety!). */
+ 
+    dtBaryons *= CourantSafetyNumber;
+    
+  }
+ 
+  /* 2) Calculate dt from particles. */
+ 
+//   if (NumberOfParticles > 0) {
+ 
+//     /* Compute dt constraint from particle velocities. */
+ 
+//     for (dim = 0; dim < GridRank; dim++) {
+//       float dCell = CellWidth[dim][0]*a;
+//       for (i = 0; i < NumberOfParticles; i++) {
+//         dtTemp = dCell/max(fabs(ParticleVelocity[dim][i]), tiny_number);
+// 	dtParticles = min(dtParticles, dtTemp);
+//       }
+//     }
+ 
+//     /* Multiply resulting dt by ParticleCourantSafetyNumber. */
+ 
+//     dtParticles *= ParticleCourantSafetyNumber;
+ 
+//   }
+ 
+  /* 3) Find dt from expansion. */
+ 
+  if (ComovingCoordinates)
+    if (CosmologyComputeExpansionTimestep(Time, &dtExpansion) == FAIL) {
+      fprintf(stderr, "nudt: Error in ComputeExpansionTimestep.\n");
+      exit(FAIL);
+    }
+ 
+  /* 4) Calculate minimum dt due to acceleration field (if present). */
+ 
+//   if (SelfGravity) {
+//     for (dim = 0; dim < GridRank; dim++)
+//       if (AccelerationField[dim])
+// 	for (i = 0; i < size; i++) {
+// 	  dtTemp = sqrt(CellWidth[dim][0]/
+// 			fabs(AccelerationField[dim][i])+tiny_number);
+// 	  dtAcceleration = min(dtAcceleration, dtTemp);
+// 	}
+//     if (dtAcceleration != HUGE_VAL)
+//       dtAcceleration *= 0.5;
+//   }
+ 
+  /* 5) calculate minimum timestep */
+ 
+  dt = min(dtBaryons, dtParticles);
+  dt = min(dt, dtViscous);
+  dt = min(dt, dtAcceleration);
+  dt = min(dt, dtExpansion);
+ 
+  /* Debugging info. */
+ 
+//   if (debug1) {
+//     printf("ComputeTimeStep = %"FSYM" (", dt);
+//     if (NumberOfBaryonFields > 0)
+//       printf("Bar = %"FSYM" ", dtBaryons);
+// //     if (HydroMethod == Zeus_Hydro)
+// //       printf("Vis = %"FSYM" ", dtViscous);
+//     if (ComovingCoordinates)
+//       printf("Exp = %"FSYM" ", dtExpansion);
+//     if (dtAcceleration != HUGE_VAL)
+//       printf("Acc = %"FSYM" ", dtAcceleration);
+//     if (NumberOfParticles)
+//       printf("Part = %"FSYM" ", dtParticles);
+//     printf(")\n");
+//   }
+ 
+  return dt;
+}
---
+++ src/Method/ppml/initialize_ppml.cpp
@@ -0,0 +1,261 @@
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+/*
+ * ENZO: THE NEXT GENERATION
+ *
+ * A parallel astrophysics and cosmology application
+ *
+ * Copyright (C) 2008 James Bordner
+ * Copyright (C) 2008 Laboratory for Computational Astrophysics
+ * Copyright (C) 2008 Regents of the University of California
+ *
+ * See CELLO_LICENSE in the main directory for full license agreement
+ *
+ */
+
+
+/** 
+ *********************************************************************
+ *
+ * @file      initialize_ppml.cpp
+ * @brief     Initialize variables in cello_hydro.h
+ * @author    James Bordner
+ * @date      Sat Aug 29 14:20:09 PDT 2009
+
+ *
+ * DESCRIPTION 
+ * 
+ *    Initialize variables in cello_hydro.h
+ *
+ * PACKAGES
+ *
+ *    NONE
+ * 
+ * INCLUDES
+ *  
+ *    cello_hydro.h
+ *
+ * PUBLIC FUNCTIONS
+ *  
+ *    initialize_ppml ();
+ *
+ * PRIVATE FUCTIONS
+ *  
+ *    NONE
+ *
+ * $Id$
+ *
+ *********************************************************************
+ */
+
+#include "cello_hydro.h"
+
+const bool debug = false;
+ 
+void initialize_ppml (int size_param, int cycles_param)
+
+{
+
+  int grid_size [] = { size_param, size_param, size_param };
+  float implosion_density_out = 1.0;
+  float implosion_density_in  = 0.125;
+  float implosion_pressure_out = 1.0;
+  float implosion_pressure_in  = 0.14;
+  float implosion_velocity_x = 0.0;
+  float implosion_velocity_y = 0.0;
+  float implosion_velocity_z = 0.0;
+
+  // Physics
+
+  Gamma                           = 1.4;
+
+  // Method PPM
+
+  PPMFlatteningParameter = 3;
+  PPMDiffusionParameter  = 1;
+  PPMSteepeningParameter = 1;
+
+  // Control
+
+  time_stop              = 10000;
+  cycle_stop             = cycles_param;
+
+  CourantSafetyNumber    = 0.8;
+  InitialRedshift        = 20;
+  InitialTimeInCodeUnits = 0;
+  Time                   = 0;
+  OldTime                = 0;
+
+  // Domain
+
+  DomainLeftEdge[0]  = 0.0;
+  DomainLeftEdge[1]  = 0.0;
+  DomainLeftEdge[2]  = 0.0;
+
+  DomainRightEdge[0] = 0.3;
+  DomainRightEdge[1] = 0.3;
+  DomainRightEdge[2] = 0.3;
+
+  // Grid
+
+  GridRank           =  3;
+  GridDimension[0]   = grid_size[0] + 6;
+  GridDimension[1]   = grid_size[1] + 6;
+  GridDimension[2]   = grid_size[2] + 6;
+  GridStartIndex[0]  = 3;
+  GridStartIndex[1]  = 3;
+  GridStartIndex[2]  = 3;
+  GridEndIndex[0]    = grid_size[0] + 3 - 1;
+  GridEndIndex[1]    = grid_size[1] + 3 - 1;
+  GridEndIndex[2]    = grid_size[2] + 3 - 1;
+  GridLeftEdge[0]    = 0.0;
+  GridLeftEdge[1]    = 0.0;
+  GridLeftEdge[2]    = 0.0;
+
+  for (int dim=0; dim<GridRank; dim++) {
+    CellWidth[dim] = new FLOAT[GridDimension[dim]];
+    float h = (DomainRightEdge[dim] - DomainLeftEdge[dim]) / 
+      (GridEndIndex[dim] - GridStartIndex[dim] + 1);
+    for (int i=0; i<GridDimension[dim]; i++) {
+      CellWidth[dim][i] = h;
+    }
+  }
+
+  // Grid variables
+  
+
+  int k = 0;
+
+  FieldType[field_density         = k++] = Density;
+  FieldType[field_total_energy    = k++] = TotalEnergy;
+  FieldType[field_velocity_x      = k++] = Velocity1;
+  FieldType[field_velocity_y      = k++] = Velocity2;
+  FieldType[field_velocity_z      = k++] = Velocity3;
+
+  //  FieldType[field_internal_energy = k++] = InternalEnergy;
+
+  NumberOfBaryonFields = k;
+  
+  int nd = GridDimension[0] * GridDimension[1] * GridDimension[2];
+
+  float * baryon_fields = new float [NumberOfBaryonFields * nd];
+  for (int field = 0; field < NumberOfBaryonFields; field++) {
+    BaryonField[field] = baryon_fields + field*nd;
+  }
+
+  float * old_baryon_fields = new float [NumberOfBaryonFields * nd];
+  for (int field = 0; field < NumberOfBaryonFields; field++) {
+    OldBaryonField[field] = baryon_fields + field*nd;
+  }
+
+  int ndx = GridDimension[0];
+  int ndy = GridDimension[1];
+  int ndz = GridDimension[2];
+
+  float xd = (DomainRightEdge[0] - DomainLeftEdge[0]) ;
+  float yd = (DomainRightEdge[1] - DomainLeftEdge[1]) ;
+  float zd = (DomainRightEdge[2] - DomainLeftEdge[2]) ;
+  int  ixg = (GridEndIndex[0] - GridStartIndex[0] + 1);
+  int  iyg = (GridEndIndex[1] - GridStartIndex[1] + 1);
+  int  izg = (GridEndIndex[2] - GridStartIndex[2] + 1);
+  float hx = CellWidth[0][0];
+  float hy = CellWidth[1][0];
+  float hz = CellWidth[2][0];
+
+  if (debug) printf ("Size = %d %d %d\n",ndx,ndy,ndz);
+  if (debug) printf ("%g  %g %g  %g %g\n",
+	  Gamma, 
+	  implosion_pressure_out,implosion_density_out,
+	  implosion_pressure_in,implosion_density_in);
+  if (debug) printf ("total energy: %g %g\n",
+	  implosion_pressure_out / ((Gamma - 1.0)*implosion_density_out),
+	  implosion_pressure_in / ((Gamma - 1.0)*implosion_density_in));
+
+  for (int iz = GridStartIndex[2]; iz<=GridEndIndex[2]; iz++) {
+
+    float z = 0.5*hz + (iz - GridStartIndex[2]) * zd / izg;
+
+    for (int iy = GridStartIndex[1]; iy<=GridEndIndex[1]; iy++) {
+
+      float y = 0.5*hy + (iy - GridStartIndex[1]) * yd / iyg;
+
+      for (int ix = GridStartIndex[0]; ix<=GridEndIndex[0]; ix++) {
+
+	float x = 0.5*hx + (ix - GridStartIndex[0]) * xd / ixg;
+
+	int i = ix + ndx * (iy + ndy * iz);
+
+	// Initialize density
+
+	if (x + y + z < 0.1517) {
+	  BaryonField[ field_density ] [ i ] = implosion_density_in;
+	} else {
+	  BaryonField[ field_density ] [ i ] = implosion_density_out;
+	}
+
+	// Initialize total energy
+
+	if (x + y + z < 0.1517) {
+	  BaryonField[ field_total_energy ][ i ] = 
+	    implosion_pressure_in / ((Gamma - 1.0)*implosion_density_in);
+	} else {
+	  BaryonField[ field_total_energy ][ i ] = 
+	    implosion_pressure_out / ((Gamma - 1.0)*implosion_density_out);
+	}
+
+	// Initialize internal energy
+
+	//      BaryonField[ field_internal_energy ][ i ] = 
+
+	// Initialize velocity
+
+	BaryonField[ field_velocity_x ][ i ] = implosion_velocity_x;
+	BaryonField[ field_velocity_y ][ i ] = implosion_velocity_y;
+	BaryonField[ field_velocity_z ][ i ] = implosion_velocity_z;
+
+      }
+    }
+  }
+
+  AccelerationField[0] = NULL;
+  AccelerationField[1] = NULL;
+  AccelerationField[2] = NULL;
+
+  // Numerics
+
+  DualEnergyFormalism             = 0;
+  DualEnergyFormalismEta1         = 0.001;
+  DualEnergyFormalismEta2         = 0.1;
+  pressure_floor                  = 1e-6;
+  number_density_floor            = 1e-6;
+  density_floor                   = 1e-6;
+  temperature_floor               = 1e-6;
+
+  // boundary
+
+  BoundaryRank = 3;
+  BoundaryDimension[0] = GridDimension[0];
+  BoundaryDimension[1] = GridDimension[1];
+  BoundaryDimension[2] = GridDimension[2];
+
+  for (int field=0; field<NumberOfBaryonFields; field++) {
+    BoundaryFieldType[field] = FieldType[field];
+    for (int dim = 0; dim < 3; dim++) {
+      for (int face = 0; face < 2; face++) {
+	int n1 = GridDimension[(dim+1)%3];
+	int n2 = GridDimension[(dim+2)%3];
+	int size = n1*n2;
+	BoundaryType [field][dim][face] = new bc_type [size];
+	BoundaryValue[field][dim][face] = NULL;
+	for (int i2 = 0; i2<n2; i2++) {
+	  for (int i1 = 0; i1<n1; i1++) {
+	    int i = i1 + n1*i2;
+	    BoundaryType[field][dim][face][i] = bc_reflecting;
+	  }
+	}
+      }
+    }
+  }
+
+}
+    
