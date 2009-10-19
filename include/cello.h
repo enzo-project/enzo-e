7f9eaeea6e123aa33cd275bf27510998841ee04f
bordner <none@none>
2009-10-19 22:44:40 +0000

    updating cello.h

--- include/cello.h
+++ include/cello.h
@@ -2,14 +2,17 @@
 #define CELLO_DEF
 
 /*********************************************************************
-* Define either CONFIG_PRECISION_SINGLE or CONFIG_PRECISION_DOUBLE
+* EDIT CONFIGURATION SETTINGS BELOW
 **********************************************************************/
 
 #define CONFIG_PRECISION_SINGLE
 /* #define CONFIG_PRECISION_DOUBLE */
 
+/* #define CONFIG_USE_MPI */
+/* #define CONFIG_USE_PAPI */
+
 /*********************************************************************
-* GLOBAL DECLARATIONS
+* PRECISION DECLARATIONS
 **********************************************************************/
 
 #ifdef CONFIG_PRECISION_SINGLE
@@ -24,31 +27,99 @@
 #   define Scalar double
 #   define SCALAR_SCANF  "%lf"
 #   define SCALAR_PRINTF "%le "
-#   define SCALAR_MPI    MPI_DOUBLE
+#   define SCALAR_MPI     MPI_DOUBLE
 #   define SCALAR_HDF5    H5T_NATIVE_DOUBLE
 #endif
 
-/* Performance attribute values for Performance::attr_component */
+/*********************************************************************
+* PERFORMANCE DECLARATIONS
+**********************************************************************/
 
-enum type_perf_group {
-  perf_group_amr,
-  perf_group_analysis,
-  perf_group_array,
-  perf_group_control,
-  perf_group_disk,
-  perf_group_error,
-  perf_group_field,
-  perf_group_memory,
-  perf_group_method,
-  perf_group_monitor,
-  perf_group_parallel,
-  perf_group_parameters,
-  perf_group_particles,
-  perf_group_performance,
-  perf_group_portal,
-  perf_group_problem,
-  perf_group_simulation
+/* Performance attributes */
+
+enum type_perf_attribute {
+  attribute_undefined, // 0 is an undefined attribute
+  attribute_timestep,  // Simulation timesteps [monotonic]
+  attribute_level,     // AMR hierarchy level
+  attribute_component, // software component [memory]
+  attribute_function,  // code function
+  num_attributes = attribute_function
 };
+
+/* Performance counters */
+
+enum type_perf_counter {
+  counter_undefined, // 0 is an undefined counter
+  time_real,           // Wallclock time of region
+  time_sim,            // Simulation time
+  mem_curr_bytes,      // Current number of bytes allocated
+  mem_high_bytes,      // Maximum number of bytes allocated
+  mem_new_count,       // Number of calls to allocate memory
+  mem_delete_count,    // Number of calls to deallocate memory
+  mem_new_bytes,       // Number of bytes allocated
+  mem_delete_bytes,    // Number of bytes deallocated
+  disk_read_bytes,     // Number of bytes read from disk
+  disk_write_bytes,    // Number of bytes written to disk
+  disk_read_time,      // Time spent reading from disk
+  disk_write_time,     // Time spent writing to disk
+  user_patch_count,    // Number of grid patches in each level
+  user_cell_count,     // Number of grid cells in each level
+  user_particle_count, // Number of particles
+
+#ifdef CONFIG_USE_MPI
+  comm_send_bytes,     // Amount of data sent from this thread
+  comm_recv_bytes,     // Amount of data sent from this thread
+  comm_send_time,      // Time spent sending data
+  comm_recv_time,      // Time spent receiving data
+  comm_global_time,    // Time spent in collective communication
+  comm_send_count,     // Number of sends
+  comm_recv_count,     // Number of receives
+  comm_global_count,   // Number of barriers/reductions
+#endif /* CONFIG_USE_MPI */
+
+#ifdef CONFIG_USE_PAPI
+  time_user,           // CPU time in user code of region
+  time_sys,            // CPU time in system of region
+  cpu_flop_count,      // Number of floating point operations
+  mem_count,           // Number of memory accesses
+#endif /* CONFIG_USE_PAPI */
+
+  num_counters = user_particle_count
+};
+
+/* Performance functions */
+
+enum type_perf_functions {
+  function_undefined, // 0 is an undefined function
+  num_functions = function_undefined
+};
+
+/*********************************************************************
+* COMPONENTS
+**********************************************************************/
+
+enum type_component {
+  component_undefined,
+  component_amr,
+  component_analysis,
+  component_array,
+  component_control,
+  component_disk,
+  component_error,
+  component_field,
+  component_memory,
+  component_method,
+  component_monitor,
+  component_parallel,
+  component_parameters,
+  component_particles,
+  component_performance,
+  component_portal,
+  component_problem,
+  component_simulation,
+  num_components
+};
+
 
 /* System includes */
 
@@ -58,11 +129,5 @@ enum type_perf_group {
 #include <string>
 #include <vector>
 #include <map>
-
-/* Include "global" components */
-
-#include "error.hpp"
-#include "performance.hpp"
-
 
 #endif /* CELLO_DEF */
