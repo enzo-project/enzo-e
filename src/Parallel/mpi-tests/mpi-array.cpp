9d5185cd1c12e966530b3b879158e6dd995fb1ef
bordner <none@none>
2009-10-20 21:16:00 +0000

    implementing mpi-array

--- src/Parallel/mpi-tests/mpi-array.cpp
+++ src/Parallel/mpi-tests/mpi-array.cpp
@@ -2,104 +2,160 @@
 #include <mpi.h>
 
 #include "error.hpp"
+#include "parameters.hpp"
 #include "performance.hpp"
 
 //----------------------------------------------------------------------
 //
 // PURPOSE
 // 
-// This is a Jacobi method test problem for evaluating different MPI
-// communication strategies on regular arrays
-//
-//----------------------------------------------------------------------
-//
-// COMMUNICATION METHOD VARIATIONS
-//
-// B    Blocking send/receive
-// I    Non-blocking send/receive
-// BB   Buffered blocking send/receive
-// BI   Buffered non-blocking send/receive
-// GF   One-sided get with fence synchronization
-// G4   One-sided get with start / complete / post / wait synchronization
-// GL   One-sided get with lock synchronization
-// PF   One-sided get with fence synchronization
-// P4   One-sided get with start / complete / post / wait synchronization
-// PL   One-sided get with lock synchronization
-// 
-//----------------------------------------------------------------------
-//
-// DATA LAYOUT VARIATIONS
-//
-// 1 in-place per-block
-// 2 in-place multi-block
-// 3 full block copy per-block
-// 4 ghost-only copy per-block
-// 5 ghost-only copy multi-block
+// This is a parallel Jacobi-like method test problem for evaluating
+// different MPI communication strategies on regular grids
 //
 //----------------------------------------------------------------------
 //
 // PARAMETERS
 //
-//  parameter    example
-// ---------    ---------
-// domain size    nx,ny,nz   [1024,1024,1024]
-// block size     bx,by,bz   [   8,   8,   8]
-// task grid      tx,ty,tz   [ 128, 128, 128]
-// processor grid px,py,pz   [   8,   8,   8]
+// mpi_type
+//    
+//     B    Blocking send/receive
+//     I    Non-blocking send/receive
+//     BB   Buffered blocking send/receive
+//     BI   Buffered non-blocking send/receive
+//     GF   One-sided get with fence synchronization
+//     G4   One-sided get with start / complete / post / wait synchronization
+//     GL   One-sided get with lock synchronization
+//     PF   One-sided get with fence synchronization
+//     P4   One-sided get with start / complete / post / wait synchronization
+//     PL   One-sided get with lock synchronization
 //
-// ghost depth    1,2,3
-// task ordering  natural         ipx + npx*(ipy + npy*ipz)
-//                hilbert
-//                hierarchical-natural
-//                hierarchical-hilbert
+// data_type          int
+//
+//      1   In-place per-block
+//      2   In-place multi-block
+//      3   Full block copy per-block
+//      4   Ghost-only copy per-block
+//      5   Ghost-only copy multi-block
+//
+// grid_size    [nx, ny, nz]
+//
+// block_size   [bx, by, bz]
+//
+// proc_size    [px, py, pz]
+//
+// ghost_depth  [gx, gy, gz]
+//
+// task_order
+//
+//      1   natural
+//      2   hilbert
+//
+// levels           [cores, cpus, nodes] (optional: for task ordering)
 //
 //----------------------------------------------------------------------
-//
-// ISSUES
-// 
-//   * performance
-//   * resiliency
-//
-//----------------------------------------------------------------------
-
 
 
 int main (int argc, char ** argv)
 {
+  //--------------------------------------------------
   // Initialize MPI
+  //--------------------------------------------------
 
   int np,ip;
   MPI_Init(&argc,&argv);
   MPI_Comm_size (MPI_COMM_WORLD, &np);
   MPI_Comm_rank (MPI_COMM_WORLD, &ip);
 
-  MPI_Status status;
+  //--------------------------------------------------
+  // Read parameter file
+  //--------------------------------------------------
 
-  if (ip==0) printf ("num_sends = %d\n",num_sends);
-  MPI_Barrier(MPI_COMM_WORLD);
-  if (ip == 0) {
-    Timer timer;
-    for (int count = 0; count < 3; count ++) {
-    for (int ip2=1; ip2 < np; ip2++) {
-      timer.start();
-      for (int i=0; i<num_sends; i++) {
-	MPI_Send(&value, 1, MPI_FLOAT, ip2, ip2*num_sends+i, MPI_COMM_WORLD);
-	MPI_Recv(&value, 1, MPI_FLOAT, ip2, ip2*num_sends+i, MPI_COMM_WORLD, &status);
-      }
-      timer.stop();
-      printf ("%d %f\n",ip2,timer.value());
-      timer.clear();
+  if (argc != 2) {
+    if (ip==0) {
+      fprintf (stderr,"Usage: %s <filename>\n",argv[0]);
     }
-    }
-
-  } else {
-    for (int count = 0; count < 3; count ++) {
-    for (int i=0; i<num_sends; i++) {
-      MPI_Recv(&value, 1, MPI_FLOAT, 0, ip*num_sends+i, MPI_COMM_WORLD, &status);
-      MPI_Send(&value, 1, MPI_FLOAT, 0, ip*num_sends+i, MPI_COMM_WORLD);
-    }
-    }
+    MPI_Abort(MPI_COMM_WORLD,1);
+    exit(1);
   }
+
+  Parameters parameters;
+
+  //--------------------------------------------------
+  // Read in input file
+  //--------------------------------------------------
+
+  FILE * fp = fopen (argv[1],"r");
+
+  if (fp == NULL) {
+    if (ip==0) {
+      fprintf (stderr,
+	       "%s:%d Error: filename %s not found\n",
+	       __FILE__,__LINE__,argv[1]);
+    }
+    MPI_Abort(MPI_COMM_WORLD,1);
+    exit(1);
+  }
+
+  parameters.read(fp);
+
+  //--------------------------------------------------
+  // Declare parameters
+  //--------------------------------------------------
+
+  std::string mpi_type;  // B I BB BI GF G4 GL PF P4 PL
+
+  int         data_type; // 1-5
+  int         task_order; // 1: natural 2: hilbert
+
+  int         nx,ny,nz;  // grid size
+  int         bx,by,bz;  // block size
+  int         px,py,pz;  // processor grid size
+  int         tx,ty,tz;  // task grid size
+
+  int         gx,gy,gz;  // ghost zone depth
+
+  int         levels[3] = {0};
+
+  //--------------------------------------------------
+  // Read parameters
+  //--------------------------------------------------
+
+  parameters.set_group ("Mpi_array");
+  mpi_type   = parameters.value_string  ("mpi_type","B");
+  data_type  = parameters.value_integer ("data_type",1);
+  task_order = parameters.value_integer ("task_order",1);
+  nx         = parameters.list_value_integer (0,"grid_size",16);
+  ny         = parameters.list_value_integer (1,"grid_size",16);
+  nz         = parameters.list_value_integer (2,"grid_size",16);
+  bx         = parameters.list_value_integer (0,"block_size",8);
+  by         = parameters.list_value_integer (1,"block_size",8);
+  bz         = parameters.list_value_integer (2,"block_size",8);
+  px         = parameters.list_value_integer (0,"proc_size",2);
+  py         = parameters.list_value_integer (1,"proc_size",2);
+  pz         = parameters.list_value_integer (2,"proc_size",1);
+  gx         = parameters.list_value_integer (0,"ghost_depth",1);
+  gy         = parameters.list_value_integer (1,"ghost_depth",1);
+  gz         = parameters.list_value_integer (2,"ghost_depth",1);
+  
+  tx = nx / bx;
+  ty = ny / by;
+  tz = nz / bz;
+
+  //--------------------------------------------------
+  // Write parameters
+  //--------------------------------------------------
+  
+    printf ("mpi_type   = %s\n",mpi_type.c_str());
+    printf ("data_type  = %d\n",data_type);
+    printf ("task_order = %d\n",task_order);
+    printf ("nx,ny,nz   = [%d %d %d]\n",nx,ny,nz);
+    printf ("bx,by,bz   = [%d %d %d]\n",bx,by,bz);
+    printf ("px,py,pz   = [%d %d %d]\n",px,py,pz);
+    printf ("tx,ty,tz   = [%d %d %d]\n",tx,ty,tz);
+    printf ("gx,gy,gz   = [%d %d %d]\n",gx,gy,gz);
+    printf ("levels     = [%d %d %d]\n",levels[0],levels[1],levels[2]);
+
+    MPI_Barrier(MPI_COMM_WORLD);
   fflush(stdout);
   MPI_Finalize();
 }
