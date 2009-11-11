29fe864b29a032307cf1319b0fbdf2b57f5c9b58
bordner <none@none>
2009-11-11 00:14:57 +0000

    Creating simulation.hpp simulation.cpp makefile

---
+++ src/Simulation/simulation.cpp
@@ -0,0 +1,136 @@
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+/** 
+ *********************************************************************
+ *
+ * @file      simulation.cpp
+ * @brief     
+ * @author    James Bordner (jobordner@ucsd.edu)
+ * @date      
+ * @bug       
+ * @note      
+ *
+ * DESCRIPTION 
+ * 
+ *    
+ *
+ * PACKAGES
+ *
+ *    
+ * 
+ * INCLUDES
+ *  
+ *    
+ *
+ * PUBLIC FUNCTIONS
+ *  
+ *    
+ *
+ * PRIVATE FUCTIONS
+ *  
+ *    
+ *
+ * $Id$
+ *
+ *********************************************************************
+ */
+ 
+//====================================================================
+// PUBLIC FUNCTIONS
+//====================================================================
+
+Simulation::Simulation()
+  : lower_(),
+    upper_(),
+    amr_(NULL),
+    parallel_(NULL),
+    fields_(NULL),
+    methods_(NULL)
+/**
+ *********************************************************************
+ *
+ * @param         
+ * @return        
+ *
+ * Create a Simulation object
+ *
+ *********************************************************************
+ */
+{
+}
+
+Simulation::~Simulation()
+/**
+ *********************************************************************
+ *
+ * @param         
+ * @return        
+ *
+ * Delete a Simulation object
+ *
+ *********************************************************************
+ */
+{
+}
+    
+/// Initialize from parameters
+void Simulation::initialize (Parameters * parameters)
+{
+  // --------------------------------------------------
+  // Initialize domain
+  // --------------------------------------------------
+
+  parameters->set_group("Domain");
+  
+  int extent_length = parameters->list_length("extent");
+  if (extent_length % 2 != 0) {
+    ERROR_MESSAGE ("Simulation::initialize",
+		   "List parameter 'Domain extent' should have an even length");
+  }
+  assert (extent_length % 2 == 0);
+  for (int i=0; i<extent_length/2; i++) {
+    lower_.push_back(parameters->list_value_integer(i));
+    upper_.push_back(parameters->list_value_integer(i+1));
+  }
+
+  // --------------------------------------------------
+  // Initialize AMR grid
+  // --------------------------------------------------
+
+  parameters->set_group("Grid");
+
+  amr_ = new Amr();
+
+  
+  //  root      = [400,400]; # size of the root grid
+  //  levels    = 4;         # maximum effective levels (for r_factor = 2)
+  //  refine    = 4;         # refinement factor
+  //  type      = tree;      # AMR type: patch or tree
+  //  full_tree = false;     # whether tree is full (ala Flash) or not
+  //  backfill  = true;      # whether to backfill for refinement > 2
+  //  coalesce  = true;      # whether to coalesce small patches to one big one
+  //  patch_min = 4;         # minimum patch size
+  //  patch_max = 128;       # maximum patch size
+
+  // --------------------------------------------------
+  // Initialize parallel object
+  // --------------------------------------------------
+
+  parallel_ = new Parallel ();
+
+  // --------------------------------------------------
+  // Initialize data fields
+  // --------------------------------------------------
+
+  fields_(NULL),
+
+  // --------------------------------------------------
+      // 
+  // --------------------------------------------------
+    methods_(NULL)
+}
+
+//====================================================================
+// PRIVATE FUNCTIONS
+//====================================================================
+
---
+++ src/Simulation/simulation.hpp
@@ -0,0 +1,131 @@
+#ifndef SIMULATION_HPP
+#define SIMULATION_HPP
+
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+/** 
+ *********************************************************************
+ *
+ * @file      simulation.hpp
+ * @brief     
+ * @author    James Bordner (jobordner@ucsd.edu)
+ * @date      
+ * @bug       
+ * @note      
+ *
+ * 
+ *
+ * $Id$
+ *
+ *********************************************************************
+ */
+
+#include <vector>
+
+#include "cello.h"
+
+#include "parameters.hpp"
+#include "amr.hpp"
+#include "field.hpp"
+#include "method.hpp"
+#include "parallel.hpp"
+
+class Simulation {
+
+/** 
+ *********************************************************************
+ *
+ * @class     Simulation
+ * @brief     
+ * @ingroup   Simulation
+ *
+ * 
+ *
+ *********************************************************************
+ */
+
+public:
+
+  //-------------------------------------------------------------------
+  // PUBLIC OPERATIONS
+  //-------------------------------------------------------------------
+
+  /// 
+  Simulation();
+
+  /// 
+  ~Simulation();
+
+  /// Initialize from parameters
+  void initialize (Parameters * parameters);
+
+  /// Return the dimension 1 <= d <= 3, of the simulation
+  int dimension() 
+  {
+    assert (lower_.size() == upper_.size()); 
+    return lower_.size(); 
+  };
+
+  /// Return extents
+  void domain_extents (int lower[], int upper[])
+  {
+    lower[0] = lower_[0];
+    lower[1] = lower_[1];
+    lower[2] = lower_[2];
+    upper[0] = upper_[0];
+    upper[1] = upper_[1];
+    upper[2] = upper_[2];
+  };
+
+  /// Return the number of fields
+  int num_fields () { return fields_.size(); };
+
+  /// Return the ith field
+  Field * field (int i) { return fields_[i]; };
+
+  /// Return the number of methods
+  int num_methods () { return methods_.size(); };
+
+  /// Return the ith method
+  Method * method (int i) { return methods_[i]; };
+
+  /// Return the parallel object
+  Parallel * parallel () { return parallel_; };
+
+  /// Return the Amr object
+  Amr * amr () { return amr_; };
+
+private:
+
+  //-------------------------------------------------------------------
+  // PRIVATE OPERATIONS
+  //-------------------------------------------------------------------
+
+  /// 
+
+private:
+
+  //-------------------------------------------------------------------
+  // PRIVATE ATTRIBUTES
+  //-------------------------------------------------------------------
+
+  /// Domain extents
+  std::vector<double> lower_;
+  std::vector<double> upper_;
+
+  /// AMR grid
+  Amr * amr_;
+
+  /// Parallelization
+  Parallel * parallel_;
+
+  /// Fields
+  std::vector<Field *> fields_;
+
+  /// Methods
+  std::vector<Method *> methods_;
+
+};
+
+#endif /* SIMULATION_HPP */
+
