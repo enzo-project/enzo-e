ea9de79bc1cd9530e44fb2317e6105df60e0f320
bordner <none@none>
2009-11-19 01:58:41 +0000

    adding missing test_ppml.h

---
+++ src/Method/ppml/test_ppml.h
@@ -0,0 +1,26 @@
+const int num_problems = 3;
+const int num_ghosts   = 3;
+
+enum type_problem {
+  problem_unknown,
+  problem_blast,
+  problem_implosion3
+};
+
+const char * problem_name[] = {
+  "",
+  "blast",
+  "implosion3"
+};
+
+const int problem_size [] = {
+  0,
+  32,
+  32
+};
+
+const int problem_cycles [] = {
+  0,
+  10000,
+  10000
+};
