d3f25cc5a4ab368276e08c59e11452ec29ffd037
bordner <none@none>
2010-02-20 00:40:40 +0000

    BUG FIX: monitor.cpp (min instead of max); comments and formatting

--- src/Monitor/monitor.cpp
+++ src/Monitor/monitor.cpp
@@ -169,7 +169,7 @@ void Monitor::image
 	// reduce along iaz axis
 	switch (op_reduce) {
 	case reduce_min: image[j] = MIN(array[i],image[j]); break;
-	case reduce_max: image[j] = MIN(array[i],image[j]); break;
+	case reduce_max: image[j] = MAX(array[i],image[j]); break;
 	case reduce_avg: image[j] += array[i]; break;
 	case reduce_sum: image[j] += array[i]; break;
 	default:         break;
--- src/Monitor/monitor.hpp
+++ src/Monitor/monitor.hpp
@@ -79,10 +79,17 @@ class Monitor {
  *********************************************************************
  *
  * @class     Monitor
- * @brief     Brief description of the class
+ * @brief     Functions for user monitoring of the execution status
  * @ingroup   Monitor
  *
- * Detailed description of the class
+ * The Monitor component is used to communicate information about the
+ * running simulation to the user. Information can be output in
+ * several forms, including text files, HTML files, plots, or other
+ * (generally small) image files. Information is assumed to be from a
+ * correctly-running simulation: anomalous errors or warnings are
+ * output by the Error component. It is assumed that stdout is not
+ * used for monitor output, except for possibly displaying header text
+ * with basic information about Cello and the simulation being run.
  *
  *********************************************************************
  */
@@ -101,30 +108,38 @@ public:
 
   /// Initialize the Monitor object
 
-  Monitor(FILE * fp = stdout) 
-    : fp_(fp)
+  Monitor(FILE * fp = stdout,
+	  bool active = true) 
+    : active_(active),
+      fp_(fp)
   {  timer_.start();};
 
   /// Print a message
+
   void print (std::string message)
   {
-    fprintf (fp_,"%6.1f %s\n",timer_.value(),message.c_str());
+    if (active_) fprintf (fp_,"%6.1f %s\n",timer_.value(),message.c_str());
   };
 
   /// Generate a PNG image of an array
+
   void image (std::string name, 
 	      Scalar * array, 
-	      int nx, int ny, int nz,  // Array dimensions
-	      int nx0, int ny0, int nz0,  // lower indices of subarray
-	      int nx1, int ny1, int nz1,  // upper indices of subarray
-	      int axis, enum_reduce op_reduce, // Axis along which to project
-	      Scalar min, Scalar max,  // Limits for color map
-	      const double * color_map, int color_length);  // Color map
+	      int nx,  int ny,  int nz,   // Array dimensions
+	      int nx0, int ny0, int nz0,  // lower inclusive subarray indices
+	      int nx1, int ny1, int nz1,  // upper exclusive subarray indices
+	      int         axis,           // Axis along which to project
+	      enum_reduce op_reduce,      // Reduction operation along axis
+	      Scalar min, Scalar max,     // Limits for color map
+	      const double * color_map,   // color map [r0 g0 b0 r1 g1 b1 ...]
+	      int            color_length // length of color map / 3
+	      );
   
 private:
 
-  FILE * fp_;
-  Timer timer_; 
+  bool   active_;  // Whether monitoring is activated.  Used for e.g. np != 0.
+  FILE * fp_;      // File pointer for message logging
+  Timer  timer_;   // Timer from Performance
 
 };
 
