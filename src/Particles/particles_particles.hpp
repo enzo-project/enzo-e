e4ccd36aa6281e11d25aba8f8d63f20e79354eed
bordner <none@none>
2010-04-02 23:10:57 +0000

    adding new particles*hpp

---
+++ src/Particles/particles_particles.hpp
@@ -0,0 +1,29 @@
+// $Id: particles.hpp 1261 2010-03-03 00:14:11Z bordner $
+// See LICENSE_CELLO file for license and copyright information
+
+#ifndef PARTICLES_PARTICLES_HPP
+#define PARTICLES_PARTICLES_HPP
+
+/// @file     particles_particles.hpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     2009-11-17
+/// @brief    Declaration for the Particles class
+
+class Particles {
+
+  /// @class    Particles
+  /// @ingroup  Particles
+  /// @brief    Interface for the Particles class
+
+public: // public
+
+  /// Initialize a Particles object
+  Particles() { };
+
+  /// Delete a Particles object
+  ~Particles() { };
+
+};
+
+#endif /* PARTICLES_PARTICLES_HPP */
+
