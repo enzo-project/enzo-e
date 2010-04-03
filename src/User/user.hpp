b6cda9170d72249023772f8bbc7e4ce07ae9583e
bordner <none@none>
2010-04-03 00:15:12 +0000

    adding user.hpp

---
+++ src/User/user.hpp
@@ -0,0 +1,15 @@
+// $Id: user.hpp 1300 2010-03-13 03:22:50Z bordner $
+// See LICENSE_CELLO file for license and copyright information
+
+#ifndef USER_HPP
+#define USER_HPP
+
+/// @file     user.hpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     2010-04-02
+/// @brief    Include file for the User component
+
+#include "user_enzo_method_ppm.hpp"
+
+#endif /* USER_HPP */
+
