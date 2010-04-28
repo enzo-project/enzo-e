359a21847a2f355c67feabd00c8f3a56ee3ca3fe
bordner <none@none>
2010-04-28 00:11:14 +0000

    updating scons files for bluedrop; removing ^M chars from PPML_MAIN

--- bin/test_ppm.sh
+++ bin/test_ppm.sh
@@ -6,13 +6,12 @@ foreach opt (yes)
    set n = 3
    while ($n <= 128)
 
-      set suffix = .optimize=$opt
      if ($n < 10) then
         set cycles = 10
      else
         set cycles = 10
      endif
-     test_ppm${suffix} implosion3 $n $cycles 0 >> out.test_ppm${suffix}
+     ./test_ppm implosion3 $n $cycles 0 >> out.test_ppm${suffix}
 
      @ n = $n + 1
    end
