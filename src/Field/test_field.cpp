2b6ddc51b5c3e50f926aae4320ee0815a250145a
bordner <none@none>
2010-05-05 18:56:35 +0000

    adding test_field

---
+++ src/Field/test_field.cpp
@@ -0,0 +1,22 @@
+// $Id: test_array.cpp 1412 2010-05-05 00:10:09Z bordner $
+// See LICENSE_CELLO file for license and copyright information
+
+/// @file     test_field.cpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     2010-05-11
+/// @brief    Test program for the Field class
+
+#include "cello.h"
+
+#include "test.hpp"
+#include "field.hpp"
+
+int main()
+{
+
+  unit_class ("Field");
+  unit_func ("Field");
+  Field * field = new Field;
+  unit_assert(field != 0);
+
+}
