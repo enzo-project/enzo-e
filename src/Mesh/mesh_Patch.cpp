15c3e3146b6691e93e354008c9f333f42d0635d2
bordner <none@none>
2010-05-10 03:08:58 +0000

    adding Patch class

---
+++ src/Mesh/mesh_Patch.cpp
@@ -0,0 +1,38 @@
+// $Id: mesh_Patch.cpp 1388 2010-04-20 23:57:46Z bordner $
+// See LICENSE_CELLO file for license and copyright information
+
+/// @file     mesh_Patch.cpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     Thu Feb 25 16:20:17 PST 2010
+/// @brief    Implementation of the Patch class
+
+#include "mesh.hpp"
+
+Patch::Patch() throw ()
+{
+}
+
+//----------------------------------------------------------------------
+
+Patch::~Patch() throw ()
+{
+}
+
+//----------------------------------------------------------------------
+
+Patch::Patch(const Patch & patch) throw ()
+/// @param     patch  Object being copied
+{
+}
+
+//----------------------------------------------------------------------
+
+Patch & Patch::operator= (const Patch & patch) throw ()
+/// @param     patch  Source object of the assignment
+/// @return    The target assigned object
+{
+  return *this;
+}
+
+//======================================================================
+
---
+++ src/Mesh/mesh_Patch.hpp
@@ -0,0 +1,39 @@
+// $Id: mesh_Patch.hpp 1394 2010-04-22 20:52:54Z bordner $
+// See LICENSE_CELLO file for license and copyright information
+
+#ifndef MESH_PATCH_HPP
+#define MESH_PATCH_HPP
+
+/// @file     mesh_Patch.hpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     Thu Feb 25 16:20:17 PST 2010
+/// @brief    Declaration of the interface for the Patch class
+
+class Patch {
+
+  /// @class    Patch
+  /// @ingroup  Mesh
+  /// @brief    Brief description of class Patch.
+
+public: // interface
+
+  /// Constructor
+  Patch() throw();
+
+  //----------------------------------------------------------------------
+  // Big Three
+  //----------------------------------------------------------------------
+
+  /// Destructor
+  ~Patch() throw();
+
+  /// Copy constructor
+  Patch(const Patch & patch) throw();
+
+  /// Assignment operator
+  Patch & operator= (const Patch & patch) throw();
+
+};
+
+#endif /* MESH_PATCH_HPP */
+
