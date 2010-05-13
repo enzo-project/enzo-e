7215fee9fcd480560071e4ee8dd7fcd2c747c6a0
bordner <none@none>
2010-05-13 05:44:14 +0000

    Fixing Ifrit format to be readable by ifrit as uniform scalar field

--- src/Disk/disk_Ifrit.cpp
+++ src/Disk/disk_Ifrit.cpp
@@ -25,10 +25,24 @@ void Ifrit::read_bin
   FILE * fp = fopen (name.c_str(), "r");
   ASSERT ("Ifrit::read_bin","int or float size in unexpected",
 	  sizeof(int)==4 && sizeof(float)==4);
+  int bound;
+  // read header
+  fread (&bound,4,1,fp);
+  if (bound != 12) WARNING_MESSAGE("Ifrit::read_bin","incorrect format");
   fread (pnx,4,1,fp);
   fread (pny,4,1,fp);
   fread (pnz,4,1,fp);
+  fread (&bound,4,1,fp);
+  if (bound != 12) WARNING_MESSAGE("Ifrit::read_bin","incorrect format");
+
+  // Read scalar field
+  int n = (*pnx)*(*pny)*(*pnz);
+  fread (&bound,4,1,fp);
+  if (bound != 4*n) WARNING_MESSAGE("Ifrit::read_bin","incorrect format");
   fread (buffer,4,(*pnx)*(*pny)*(*pnz),fp);
+  fread (&bound,4,1,fp);
+  if (bound != 4*n) WARNING_MESSAGE("Ifrit::read_bin","incorrect format");
+
   fclose (fp);
 }
 
@@ -45,10 +59,20 @@ void Ifrit::write_bin
   FILE * fp = fopen (name.c_str(), "w");
   ASSERT ("Ifrit::read_bin","int or float size in unexpected",
 	  sizeof(int)==4 && sizeof(float)==4);
+  // Write header
+  int bound = 12;
+  fwrite (&bound,4,1,fp);
   fwrite (&nx,4,1,fp);
   fwrite (&ny,4,1,fp);
   fwrite (&nz,4,1,fp);
+  fwrite (&bound,4,1,fp);
+
+  // Write scalar field
+  bound = 4*nx*ny*nz;
+  fwrite (&bound,4,1,fp);
   fwrite (buffer,4,nx*ny*nz,fp);
+  fwrite (&bound,4,1,fp);
+
   fclose (fp);
 }
 
