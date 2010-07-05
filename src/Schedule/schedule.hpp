5cdecd40fbfdcce6655f7ae3b08712579979176c
bordner <none@none>
2010-07-05 20:57:01 +0000

    deleting Schedule

--- src/Schedule/schedule.hpp
+++ src/Schedule/schedule.hpp
@@ -9,40 +9,6 @@
 /// @date     2009-11-10 16:12:58
 /// @brief    Interface for the Schedule class
 
-#include <stdio.h>
-
-#include "simulation.hpp"
-#include "parameters.hpp"
-
-class Schedule {
-
-  /// @class    Schedule
-  /// @ingroup  Schedule
-  /// @brief    Class for controlling scheduling of parallel tasks
-
-public: // interface
-
-  /// Initialize a Schedule object
-  Schedule();
-
-  /// Create the simulation
-  void create_simulation();
-
-  /// Initialize the simulation
-  void initialize_simulation();
-
-  /// Run the simulation
-  void execute_simulation();
-
-  /// Terminate the simulation
-  void terminate_simulation();
-
-private: // attributes
-
-  /// The simulation we're scheduling
-  Simulation * simulation_;
-
-};
 
 #endif /* SCHEDULE_HPP */
 
