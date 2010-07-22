ca69c7647b40361fa2d3ebb2af88c66b5bd0cff1
bordner <none@none>
2010-07-22 19:39:03 +0000

    adding missing schedule_Schedule.hpp

---
+++ src/Schedule/schedule_Schedule.hpp
@@ -0,0 +1,30 @@
+// $Id: schedule_Schedule.hpp 1394 2010-04-22 20:52:54Z bordner $
+// See LICENSE_CELLO file for license and copyright information
+
+#ifndef SCHEDULE_SCHEDULE_HPP
+#define SCHEDULE_SCHEDULE_HPP
+
+/// @file     schedule_Schedule.hpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     Thu Feb 25 16:20:17 PST 2010
+/// @brief    Brief description of file schedule_Schedule.hpp
+
+class Schedule {
+
+  /// @class    Schedule
+  /// @ingroup  Schedule
+  /// @brief    Brief description of class Schedule.
+
+public: // interface
+
+
+  //----------------------------------------------------------------------
+
+private: // functions
+
+private: // attributes
+
+};
+
+#endif /* SCHEDULE_SCHEDULE_HPP */
+
