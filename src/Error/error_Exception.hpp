c67078f892d6d3c9c9bb785dd8fc3cd806e61926
bordner <none@none>
2010-11-05 21:11:40 +0000

    adding components.text and classes.text; removing unused exception classes

--- src/Error/error_Exception.hpp
+++ src/Error/error_Exception.hpp
@@ -19,16 +19,6 @@ class Exception { };
 /// @brief    Global exception class for encountering a bad pointer
 class ExceptionBadPointer           : public Exception {};
 
-/// @class    ExceptionBadArrayDeallocation
-/// @ingroup  Error
-/// @brief    Array exception class for bad deallocation
-class ExceptionBadArrayDeallocation : public Exception {};
-
-/// @class    ExceptionBadArrayAllocation
-/// @ingroup  Error
-/// @brief    Array exception class for bad allocation
-class ExceptionBadArrayAllocation   : public Exception {};
-
 /// @class    ExceptionMemoryBadDeallocate
 /// @ingroup  Error
 /// @brief    Memory exception class for bad deallocation
