b5e1d9c8b6c21ddeba21777f7c91d7156b656a5d
bordner <none@none>
2010-12-16 19:56:05 +0000

    adding missing MethodInitial files

---
+++ src/Method/method_MethodInitial.cpp
@@ -0,0 +1,62 @@
+// $Id: method_MethodInitial.cpp 1896 2010-12-03 23:54:08Z bordner $
+// See LICENSE_CELLO file for license and copyright information
+
+/// @file     method_MethodInitial.cpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     Mon Jul 13 11:12:25 PDT 2009
+/// @brief    Implements the MethodInitial base class
+
+#include <string>
+
+#include "cello.hpp"
+
+#include "method.hpp"
+#include "field.hpp"
+
+//----------------------------------------------------------------------
+
+void MethodInitial::add_argument_
+(
+ argument_enum argument,
+ std::string   argument_name,
+ access_enum   access_type,
+ DataDescr   * data_descr
+ ) throw()
+/// @param    argument       Type of argument, field or particle
+/// @param    argument_name  Name of the argument, e.g. "Density"
+/// @param    access_type    Access type of the argument, e.g. read, write
+/// @param    data_descr     The data descriptor
+{
+
+  // Monitor output
+
+  char buffer[100];
+  sprintf (buffer,"Method %s: adding %s", 
+	   method_name().c_str(), 
+	   argument_name.c_str());
+  global_->monitor()->print (buffer);
+
+  // Add method argument information
+  argument_types_.push_back(argument);
+  argument_names_.push_back(argument_name);
+  access_types_.push_back  (access_type);
+
+  // If data_descr is passed in (default = 0), then verify that the argument
+  // is defined
+  if (data_descr) {
+    char buffer [ ERROR_MESSAGE_LENGTH ];
+    switch (argument) {
+    case argument_field:
+      sprintf (buffer, 
+	       "Required Field %s is not defined in the field descriptor",
+	       argument_name.c_str());
+      ASSERT("MethodInitial::initialize_method",
+	     buffer, data_descr->field_descr()->is_field(argument_name));
+      break;
+    case argument_particle:
+    case argument_unknown:
+      break;
+    }
+  }
+
+}
---
+++ src/Method/method_MethodInitial.hpp
@@ -0,0 +1,76 @@
+// $Id: method_MethodInitial.hpp 1896 2010-12-03 23:54:08Z bordner $
+// See LICENSE_CELLO file for license and copyright information
+
+/// @file     method_MethodInitial.hpp 
+/// @author   James Bordner (jobordner@ucsd.edu) 
+/// @date     Mon Jul 13 11:11:47 PDT 2009 
+/// @todo     Common ABC for MethodInitial and MethodHyperbolic
+/// @brief    Declaration for the MethodInitial component
+
+#ifndef METHOD_METHOD_INITIAL_HPP
+#define METHOD_METHOD_INITIAL_HPP
+
+class MethodInitial {
+
+  /// @class    MethodInitial
+  /// @ingroup  Method
+  /// @brief    Encapsulate external method / analysis / visualization function.
+
+public: // interface
+
+  /// Create a new MethodInitial
+  MethodInitial(Global * global) throw()
+    : global_(global)
+  {};
+
+public: // virtual functions
+
+  /// Perform any method-specific initialization
+
+  virtual void initialize (DataDescr * data_descr) throw() = 0;
+
+  /// Perform any method-specific finalizations steps, e.g. to
+  /// deallocate any dynamically-allocated memory
+
+  virtual void finalize (DataDescr * data_descr) throw() = 0;
+
+  /// Initialize PPM variable that may change.  Called once per
+  /// block per timestep.
+
+  virtual void initialize_block (DataBlock * data_block) throw() = 0;
+
+  /// Finalize PPM after advancing a block a timestep, e.g. to deallocate
+  /// any dynamically-allocated variables
+
+  virtual void finalize_block (DataBlock * data_block) throw() = 0;
+
+  /// Return the name of the method
+
+  virtual std::string method_name() const throw() = 0;
+
+protected: // functions
+
+  /// Specify a field or particle type and its access type
+
+  void add_argument_(argument_enum type,
+		     std::string   name,
+		     access_enum   access_type,
+		     DataDescr   * data_descr = 0) throw();
+
+protected: // attributes
+
+  /// Global
+  Global * global_;
+
+  /// List of argument types, e.g. argument_type_field
+  std::vector<argument_enum> argument_types_;
+
+  /// List of argument names, e.g. "Density", "Velocity-X", etc.
+  std::vector<std::string>   argument_names_;
+
+  /// List of argument access types, e.g. access_read_write
+  std::vector<access_enum>   access_types_;
+
+};
+
+#endif /* METHOD_METHOD_METHOD_HPP */
