2ff89bc37c7b7187d19fc0599eb800ec5e9013fc
James Bordner <jobordner@ucsd.edu>
2011-09-01 15:47:45 -0700

    Adding missing Io files

---
+++ src/Io/io.hpp
@@ -0,0 +1,41 @@
+// See LICENSE_CELLO file for license and copyright information
+
+#ifndef IO_HPP
+#define IO_HPP
+
+/// @file     io.hpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     2011-11-01
+/// @brief    Include file for the \ref Io component 
+
+//----------------------------------------------------------------------
+// System includes
+//----------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------
+// Component dependencies
+//----------------------------------------------------------------------
+
+#include "cello.hpp"
+
+#include "parallel.hpp"
+#include "memory.hpp"
+
+#include "simulation.hpp"
+#include "mesh.hpp"
+#include "disk.hpp"
+#include "field.hpp" 
+
+//----------------------------------------------------------------------
+// Component class includes
+//----------------------------------------------------------------------
+
+#include "io_Io.hpp"
+#include "io_IoSimulation.hpp"
+#include "io_ItField.hpp"
+#include "io_Output.hpp"
+#include "io_OutputImage.hpp"
+
+#endif /* IO_HPP */
+
