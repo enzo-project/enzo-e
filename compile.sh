0612b46ab013e093b66e7adb9e4faa493b5dee9a
James Bordner <jobordner@ucsd.edu>
2011-11-09 14:07:45 -0800

    Scons: added mpi_type, added bin,lib,include_path

--- compile.sh
+++ compile.sh
@@ -10,17 +10,20 @@ set proc = 8
 # set PREC = (single double)
 
 # set default target
-set target = "compile"
+set target = "install-bin"
 
 if ($#argv >= 1) then
    if ($argv[1] == "clean") then
       set d = `date +"%Y-%m-%d %H:%M:%S"`
       printf "$d %-14s" "cleaning..."
       foreach type (serial mpi charm)
+      foreach prec (single double)
+         set conf = $type
          scons arch=$ARCH type=$type -c >& /dev/null
-         rm -rf test/$type >& /dev/null
-         rm -rf bin/$type >& /dev/null
-         rm -rf lib/$type >& /dev/null
+         rm -rf test/$conf >& /dev/null
+         rm -rf bin/$conf >& /dev/null
+         rm -rf lib/$conf >& /dev/null
+      end
       end
       rm -rf include >& /dev/null
       rm -f test/COMPILING
@@ -29,7 +32,8 @@ if ($#argv >= 1) then
       printf "done\n"
       exit
    else if ($argv[1] == "enzo-p") then
-     set target = bin/$type/enzo-p
+     set conf = $type
+     set target = bin/$conf/enzo-p
    else if ($argv[1] == "compile") then
      set target = install-bin
    else if ($argv[1] == "test") then
@@ -52,20 +56,22 @@ foreach arch ($ARCH)
 foreach type ($TYPE)
 foreach prec ($PREC)
 
+   set conf = $type
+
    rm -f "test/*/running.$arch.$prec"
 
    set configure = $arch-$type-$prec
    set configure_print = `printf "%s %s %s" $arch $type $prec`
 
 
-   printf "$type" > test/COMPILING
+   printf "$configure" > test/COMPILING
 
    # clean
 #  scons arch=$arch type=$type prec=$prec -c >& /dev/null
 
    # make output directory for compilation and tests
 
-   set dir = test/$type
+   set dir = test/$conf
    if (! -d $dir) mkdir $dir
 
    # COMPILE
