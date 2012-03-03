bc9800232fe26d0c29f4ca0524c5f63b5d77c554
James Bordner <jobordner@ucsd.edu>
2012-03-03 12:06:20 -0800

    adding missing problem.hpp

---
+++ src/Cello/problem.hpp
@@ -0,0 +1,25 @@
+// See LICENSE_CELLO file for license and copyright information
+
+/// @file     problem.hpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     Thu Mar 11 17:20:03 PST 2010
+/// @brief    Include file for the \ref Problem component
+
+#ifndef PROBLEM_HPP
+#define PROBLEM_HPP
+
+//----------------------------------------------------------------------
+// System includes
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+// Component dependencies
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+// Component class includes
+//----------------------------------------------------------------------
+
+#include "problem_Problem.hpp"
+
+#endif /* PROBLEM_HPP */
