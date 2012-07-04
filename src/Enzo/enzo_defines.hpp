a3be66780172f88176a7e74a478cdf7deb04e67e
James Bordner <jobordner@ucsd.edu>
2012-07-04 16:45:17 -0700

    reducing MAX_ANY_SINGLE_DIRECTION

--- src/Enzo/enzo_defines.hpp
+++ src/Enzo/enzo_defines.hpp
@@ -17,7 +17,7 @@
 
 #define MAX_COLOR                20
 #define huge                     1.0e+20
-#define MAX_ANY_SINGLE_DIRECTION 4200+6
+#define MAX_ANY_SINGLE_DIRECTION 1024+6
 #define tiny                     1.0e-20
 
 #define NUM_FIELDS 34
