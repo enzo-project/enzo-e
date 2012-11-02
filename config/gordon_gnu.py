dbb7c5a13dbcd87e06779f305ab9ae148afab101
ux452912@gordon-ln2.sdsc.edu <ux452912@gordon-ln2.sdsc.edu>
2012-11-02 11:02:00 -0700

    porting to gordon

---
+++ config/gordon_gnu.py
@@ -0,0 +1,25 @@
+f90 = {}
+cxx = {}
+cc  = {}
+
+is_arch_valid = 1
+
+# flags_arch = '-g -Wall'
+flags_arch = '-O3'
+flags_link  = '-rdynamic'
+
+cc['mpi']     = 'mpicc'
+cc['serial']  = 'gcc'
+cxx['mpi']    = 'mpiCC'
+cxx['serial'] = 'g++'
+f90['charm']  = 'gfortran'
+f90['mpi']    = 'gfortran'
+f90['serial'] = 'gfortran'
+
+libpath_fortran = ''
+libs_fortran    = ['gfortran']
+
+charm_path = '/home/ux452912/charm/charm'
+
+papi_path  = '/home/ux452912'
+hdf5_path  = '/opt/hdf5/gnu'
