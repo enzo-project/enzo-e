9177f2c1fbaaae519d165ce1123f4362ca3014b8
bordner@h2ologin1 <bordner@h2ologin1>
2013-01-15 12:44:28 -0600

    adding local scons; updating ncsa.bw

--- config/ncsa_bw.py
+++ config/ncsa_bw.py
@@ -15,12 +15,12 @@ f90['charm']  = 'ftn -rm'
 f90['mpi']    = 'ftn -rm'
 f90['serial'] = 'ftn -rm'
 
-libpath_fortran = '/home/cpv/tra61/lib'
+libpath_fortran = ''
 libs_fortran    = []
 
-charm_path = '/home/cpv/tra61/charm'
-papi_path  = ''
-hdf5_path  = '/opt/cray/hdf5/1.8.6/cray/73'
+charm_path = '/u/sciteam/bordner/Charm/charm'
+papi_path  = '/u/sciteam/bordner'
+hdf5_path  = '/opt/cray/hdf5/default/cray/74'
 
 if (type == "mpi"):
    parallel_run = "aprun -n 8"
