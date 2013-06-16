8da37bae35cbf8ff230f822fb1543725ff73e7be
James Bordner [ gedeckt ] <none@none>
2013-06-16 09:51:34 -0700

    added missing EnzoInitialSedovArray2  files
    --HG--
    branch : add-refine

---
+++ src/Enzo/enzo_EnzoInitialSedovArray2.cpp
@@ -0,0 +1,192 @@
+// See LICENSE_CELLO file for license and copyright information
+
+/// @file     enzo_EnzoInitialSedovArray2.cpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     Sat Jun 15 13:44:27 PDT 2013
+/// @brief    Implementation of an array of Sedov problems, one per Block
+///
+/// This problem is designed for weak-scaling studies.  Each block
+/// contains a small Sedov blast problem.  Problem-specific parameters
+/// are used to define the initial values, and whether all blasts go
+/// at once or intermitently
+
+#include "cello.hpp"
+
+#include "enzo.hpp"
+
+//----------------------------------------------------------------------
+
+EnzoInitialSedovArray2::EnzoInitialSedovArray2 
+(const EnzoConfig * config) throw ()
+: Initial(config->initial_cycle, config->initial_time) ,
+  hydro_(hydro_unknown)
+{
+  array_[0]        = config->enzo_sedov_array[0];
+  array_[1]        = config->enzo_sedov_array[1];
+  radius_relative_ = config->enzo_sedov_radius_relative;
+  pressure_in_     = config->enzo_sedov_pressure_in;
+  pressure_out_    = config->enzo_sedov_pressure_out;
+  density_         = config->enzo_sedov_density;
+
+  if (config->method_sequence[0] == "ppm") {
+    hydro_ = hydro_ppm;
+    TRACE("hydro_ppm");
+  } else if (config->method_sequence[0] == "ppml") {
+    hydro_ = hydro_ppml;
+    TRACE("hydro_ppml");
+  } else {
+    ERROR1("EnzoInitialSedovArray2::EnzoInitialSedovArray2",
+	   "Unknown method %s",config->method_sequence[0].c_str());
+  }
+					  
+}
+
+//----------------------------------------------------------------------
+
+#ifdef CONFIG_USE_CHARM
+
+void EnzoInitialSedovArray2::pup (PUP::er &p)
+{
+  // NOTE: update whenever attributes change
+
+  TRACEPUP;
+
+  Initial::pup(p);
+
+  p | radius_relative_;
+  p | pressure_in_;
+  p | pressure_out_;
+  p | density_;
+  p | hydro_;
+}
+#endif
+
+//----------------------------------------------------------------------
+void EnzoInitialSedovArray2::enforce_block
+(
+ CommBlock * comm_block,
+ const FieldDescr * field_descr,
+ const Hierarchy  * hierarchy
+ ) throw()
+
+{
+
+  ASSERT("EnzoInitialSedovArray2",
+	 "CommBlock does not exist",
+	 comm_block != NULL);
+
+  FieldBlock * field_block = comm_block->block()->field_block();
+
+  ASSERT("EnzoInitialSedovArray2",
+	 "Insufficient number of fields",
+	 field_descr->field_count() >= 4);
+
+  enzo_float *  d = (enzo_float *) field_block->field_values
+    (field_descr->field_id("density"));
+  
+  enzo_float * te = 0;
+  if (hydro_ == hydro_ppm) 
+    te = (enzo_float *) field_block->field_values
+      (field_descr->field_id("total_energy"));
+
+  int nx,ny;
+  field_block->size(&nx,&ny);
+
+  double xbm,ybm;
+  comm_block->block()->lower(&xbm,&ybm);
+
+  double xbp,ybp;
+  comm_block->block()->upper(&xbp,&ybp);
+
+  double hx,hy;
+  field_block->cell_width(xbm,xbp,&hx,
+			  ybm,ybp,&hy);
+
+  // Parameters
+
+  const double sedov_radius = radius_relative_/array_[0];
+  const double sedov_radius_2 = sedov_radius*sedov_radius;
+
+  const double sedov_te_in = 
+    pressure_in_  / ((EnzoBlock::Gamma - 1.0) * density_);
+  const double sedov_te_out= 
+    pressure_out_ / ((EnzoBlock::Gamma - 1.0) * density_);
+
+  int gx,gy;
+  field_descr->ghosts(0,&gx,&gy);
+
+  int ndx = nx + 2*gx;
+  int ndy = ny + 2*gy;
+
+  // clear all fields
+
+  for (int iv=0; iv<field_descr->field_count(); iv++) {
+
+    enzo_float * field = (enzo_float *) field_block->field_values (iv);
+
+    for (int iy=0; iy<ndy; iy++) {
+      for (int ix=0; ix<ndx; ix++) {
+	int i = INDEX2(ix,iy,ndx);
+	field[i] = 0.0;
+      }
+    }
+  }
+
+  // background 
+
+  for (int iy=0; iy<ndy; iy++) {
+    for (int ix=0; ix<ndx; ix++) {
+
+      int i = INDEX2(ix,iy,ndx);
+      d[i]  = density_;
+      if (hydro_ == hydro_ppm) te[i] = sedov_te_out;
+
+    }
+  }
+
+  // array of explosions
+
+  // (kx,ky) index of explosion in domain
+  // (x,y) position in block
+  int kxm = 0;
+  int kym = 0;
+  int kxp = array_[0];
+  int kyp = array_[1];
+  TRACE2 ("SEDOV: %d %d",kxp,kyp);
+
+  double xdm,ydm;
+  hierarchy->lower(&xdm,&ydm);
+  double xdp,ydp;
+  hierarchy->upper(&xdp,&ydp);
+
+  double hxa = (xdp-xdm) / array_[0];
+  double hya = (ydp-ydm) / array_[1];
+
+  for (int ky=kym; ky<kyp; ky++) {
+    double yc = hya*(0.5+ky);
+    for (int kx=kxm; kx<kxp; kx++) {
+      double xc = hxa*(0.5+kx);
+      TRACE2("xc,yc = %f %f %f",xc,yc);
+
+      // (explosion center xc,yc)
+
+      for (int iy=0; iy<ndy; iy++) {
+	double y = ybm + (iy - gy + 0.5)*hy - yc;
+	for (int ix=0; ix<ndx; ix++) {
+	  double x = xbm + (ix - gx + 0.5)*hx - xc;
+	  double r2 = x*x + y*y;
+
+	  int i = INDEX2(ix,iy,ndx);
+	  
+	  if (r2 < sedov_radius_2) {
+	    if (hydro_ == hydro_ppm)  te[i] = sedov_te_in;
+	    if (hydro_ == hydro_ppml) d[i] = density_ * 
+					(sedov_te_in / sedov_te_out);
+	  }
+	}
+      }
+    }
+
+  }
+}
+
---
+++ src/Enzo/enzo_EnzoInitialSedovArray2.hpp
@@ -0,0 +1,64 @@
+// See LICENSE_CELLO file for license and copyright information
+
+/// @file     enzo_EnzoInitialSedovArray2.hpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     Sat Jun 15 13:43:39 PDT 2013
+/// @brief    [\ref Enzo] Initialization routine for 2D implosion problem
+
+#ifndef ENZO_ENZO_INITIAL_SEDOV_ARRAY2_HPP
+#define ENZO_ENZO_INITIAL_SEDOV_ARRAY2_HPP
+
+class EnzoInitialSedovArray2 : public Initial {
+
+  /// @class    EnzoInitialSedovArray2
+  /// @ingroup  Enzo
+  /// @brief    [\ref Enzo] Initialization routine for 2D implosion problem
+
+public: // interface
+
+  /// CHARM++ constructor
+  EnzoInitialSedovArray2() throw() { }
+  
+  /// Constructor
+  EnzoInitialSedovArray2(const EnzoConfig * enzo_config) throw();
+
+#ifdef CONFIG_USE_CHARM
+
+  /// CHARM++ PUP::able declaration
+  PUPable_decl(EnzoInitialSedovArray2);
+
+  /// CHARM++ migration constructor
+  EnzoInitialSedovArray2(CkMigrateMessage *m) : Initial (m) {}
+
+  /// CHARM++ Pack / Unpack function
+  void pup (PUP::er &p);
+
+#endif
+
+  /// Initialize the block
+
+  virtual void enforce_block
+  ( CommBlock * block, const FieldDescr * field_descr, const Hierarchy * hierarchy ) throw();
+
+private: // attributes
+
+  /// Size of the array of Sedov blasts
+  int array_[2];
+
+  /// Relative radius
+  double radius_relative_;
+
+  /// Internal and external pressure
+  double pressure_in_;
+  double pressure_out_;
+
+  /// initial density
+  double density_;
+
+  /// Whether PPM or PPML is used
+  int hydro_;
+
+};
+
+#endif /* ENZO_ENZO_INITIAL_SEDOV_ARRAY2_HPP */
+
