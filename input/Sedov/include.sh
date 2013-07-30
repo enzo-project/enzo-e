a5350dc50f2efa42c021d4f16cbddfc2c51058a1
James Bordner [ gordon ] <none@none>
2013-07-29 17:19:14 -0700

    updating include.sh
    --HG--
    branch : add-refine

--- input/Sedov/include.sh
+++ input/Sedov/include.sh
@@ -1,14 +1,27 @@
-#INPUT
-# P=%04d     number of processors
-# T=[2a|3a]  type: 2D amr or unigrid
+#----------------------------------------------------------------------
+# INPUT FROM CALLING SCRIPT
+#
+#    P=%04d     number of processors
+#    T=[2a|3a]  type: 2D amr or unigrid
+#
+#----------------------------------------------------------------------
+# FILES
+#
+#  CELLO: e.g. cello-src.infiniband
+#  CHARM: e.g. 650/gnu/infiniband/charm-6.5.0
+#
+#----------------------------------------------------------------------
 
-cd $HOME/Cello/cello-src/input/Sedov
+cello=$HOME/Cello/`cat CELLO`
+charm=$HOME/charm/`cat CHARM`
 
-enzorun=$HOME/Cello/cello-src/bin/enzo-p
-charmrun=$HOME/Charm/charm/bin/charmrun
+cd $cello/input/Sedov
+
+enzorun=$cello/bin/enzo-p
+charmrun=$charm/bin/charmrun
 input=input/sedov$T$P.in
 output=out.sedov$T$P
 
-$charmrun ++mpiexec +p$P bin/charm/enzo-p sedov$T$P.in >& out.sedov$T$P
+$charmrun ++mpiexec +p$P $enzorun $input >& $output
 
 
