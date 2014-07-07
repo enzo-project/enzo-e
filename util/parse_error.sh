2401d99eeebee89a4a8047e19c64d5d32f043dec
James Bordner [ gedeckt ] <none@none>
2014-07-07 08:36:44 -0700

    added warnings to parse_error.sh

--- util/parse_error.sh
+++ util/parse_error.sh
@@ -4,6 +4,10 @@
 #
 # output: errors.org with links to code
 
-echo "* ERRORS [/]" > errors.org
-awk 'BEGIN{c=0}; /error:/{c=c+1; split($1,a,":"); b="";  sub(/build/, "src",a[1]); sub(/include/,"src/Cello",a[1]); sub(/\[/,"(",$0); sub(/\]/,")",$0); for (i=3; i<=NF; i++) b = (b " " $i);  print "** TODO [[file:"a[1]"::"a[2]"][Error "c": " b"]]"}' test/out.scons >> errors.org
+echo "* [[file:test/out.scons][ERRORS]] [/]" > errors.org
+awk 'BEGIN{c=0}; /error:/{c=c+1; split($1,a,":"); b="";  sub(/build/, "src",a[1]); sub(/include/,"src/Cello",a[1]); for (i=3; i<=NF; i++) { sub(/\[/,"(",$i); sub(/\]/,")",$i); b = (b " " $i); } print "** TODO [[file:"a[1]"::"a[2]"][Error "c": " b"]]"}' test/out.scons >> errors.org
+
+echo "* [[file:test/out.scons][WARNINGS]] [/]" >> errors.org
+awk 'BEGIN{c=0}; /warning:/{c=c+1; split($1,a,":"); b="";  sub(/build/, "src",a[1]); sub(/include/,"src/Cello",a[1]); for (i=3; i<=NF; i++) { sub(/\[/,"(",$i); sub(/\]/,")",$i); b = (b " " $i); } print "** TODO [[file:"a[1]"::"a[2]"][Warning "c": " b"]]"}' test/out.scons >> errors.org
+
 
