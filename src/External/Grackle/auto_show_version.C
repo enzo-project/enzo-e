2ce2579139642cf0c8d813d028567179c888e09e
James Bordner [ gedeckt ] <none@none>
2014-07-08 15:49:10 -0700

    removed Grackle from repository

--- src/External/Grackle/auto_show_version.C
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void auto_show_version(FILE *fp) {
-   fprintf (fp,"\n");
-   fprintf (fp,"Mercurial Branch   default\n");
-   fprintf (fp,"Mercurial Revision f1324bf57c78\n");
-   fprintf (fp,"Library Release Version 1.0\n");
-   fprintf (fp,"\n");
-}
