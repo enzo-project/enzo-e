1946956d84eee25db7541bf6bb435892675cadbd
James Bordner [ gedeckt ] <none@none>
2015-09-15 09:53:55 -0700

    added missing problem_Method.cpp

---
+++ src/Cello/problem_Method.cpp
@@ -0,0 +1,30 @@
+// See LICENSE_CELLO file for license and copyright information
+
+/// @file     problem_Method.cpp
+/// @author   James Bordner (jobordner@ucsd.edu)
+/// @date     2015-09-04
+/// @brief    
+
+#include "problem.hpp"
+
+//----------------------------------------------------------------------
+
+Method::~Method() throw()
+{
+  delete schedule_;
+  for (size_t i=0; i<refresh_list_.size(); i++) {
+    delete refresh_list_[i];
+    refresh_list_[i] = 0;
+  }
+}
+
+//----------------------------------------------------------------------
+
+void Method::set_schedule (Schedule * schedule) throw()
+{ 
+  if (schedule_) delete schedule_;
+  schedule_ = schedule;
+}
+
+//======================================================================
+
