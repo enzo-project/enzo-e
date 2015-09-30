8a67b0a5a009cf2c83519a846d63546074b24967
james_larrue <none@none>
2015-09-29 23:22:52 -0400

    Added /home/jamesl/src/ and /home/jamesl/source/ as potential locations for Charm++ and Grackle.  I was told by some other Enzo developers that they checkout their software to those locations.

--- config/linux_gnu.py
+++ config/linux_gnu.py
@@ -26,6 +26,10 @@ if charm_path is None:
 			charm_path = home + '/Charm/charm'
 		elif os.path.isdir(home + '/charm'):
 			charm_path = home + '/charm'
+		elif os.path.isdir(home + '/src/charm'):
+			charm_path = home + '/src/charm'
+		elif os.path.isdir(home + '/source/charm'):
+			charm_path = home + '/source/charm'
 	if charm_path is None:
 		if os.path.isdir('/usr/local/charm'):
 			charm_path = '/usr/local/charm'
@@ -45,6 +49,10 @@ if grackle_path is None:
 			grackle_path = home + '/Grackle/src/clib'
 		elif os.path.isdir(home + '/grackle/src/clib'):
 			grackle_path = home + '/grackle/src/clib'
+		elif os.path.isdir(home + '/src/grackle/src/clib'):
+			grackle_path = home + '/src/grackle/src/clib'
+		elif os.path.isdir(home + '/source/grackle/src/clib'):
+			grackle_path = home + '/source/grackle/src/clib'
 		elif os.path.isdir(home + '/Software/Grackle/src/clib'):
 			grackle_path = home + '/Software/Grackle/src/clib'
 	if grackle_path is None:
