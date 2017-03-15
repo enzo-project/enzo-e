3e1b3b2ba8f4a489354b5a93cbd96c463d72730f
James Bordner [ Blue Waters ] <none@none>
2017-03-15 13:58:30 -0500

    updating README.ncsa_bw
    --HG--
    branch : solver

--- config/ncsa_bw.py
+++ config/ncsa_bw.py
@@ -1,3 +1,7 @@
+#
+# Enzo-P/Cello Configuration file for NCSA Blue Waters
+#
+# See README.ncsa_bw for additional information
 
 import os
 
@@ -11,8 +15,8 @@ node_size = 32 # for BW integer cores
 
 is_arch_valid = 1
 
-flags_arch = '-O3'
-flags_link  = ''
+flags_arch = '-O3 -std=gnu++11'
+flags_link  = '-std=gnu++11'
 
 flags_prec_single = ''
 flags_prec_double = ''
@@ -26,8 +30,7 @@ libs_fortran    = ['gfortran']
 home = os.environ['HOME']
 
 charm_path = home + '/Charm/charm'
-png_path   = home
-#papi_path  = home
+png_path   = ''
 hdf5_path  = os.environ["HDF5_DIR"]
 hdf5_inc = hdf5_path + '/include'
 hdf5_lib = hdf5_path + '/lib'
