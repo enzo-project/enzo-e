a6fa8da15d19bda0b7847d1a4b393fdd94a03bed
James Bordner [ gedeckt ] <none@none>
2018-01-23 14:21:20 -0800

    Adding support for optional ghost zone layer in ProlongLinear

--- src/Cello/problem_ProlongLinear.cpp
+++ src/Cello/problem_ProlongLinear.cpp
@@ -19,32 +19,32 @@ ProlongLinear::ProlongLinear() throw()
 
 int ProlongLinear::apply 
 ( precision_type precision,
-  void *       values_f, int nd3_f[3], int im3_f[3], int n3_f[3],
-  const void * values_c, int nd3_c[3], int im3_c[3], int n3_c[3])
+  void *       values_f, int mf3[3], int of3[3], int nf3[3],
+  const void * values_c, int mc3[3], int oc3[3], int nc3[3])
 {
   TRACE6("ProlongLinear fine   %d:%d %d:%d %d:%d",
-         im3_f[0],n3_f[0]+im3_f[0],
-         im3_f[1],n3_f[1]+im3_f[1],
-         im3_f[2],n3_f[2]+im3_f[2]);
+         of3[0],nf3[0]+of3[0],
+         of3[1],nf3[1]+of3[1],
+         of3[2],nf3[2]+of3[2]);
 
   TRACE6("ProlongLinear coarse %d:%d %d:%d %d:%d",
-         im3_c[0],n3_c[0]+im3_c[0],
-         im3_c[1],n3_c[1]+im3_c[1],
-         im3_c[2],n3_c[2]+im3_c[2]);
+         oc3[0],nc3[0]+oc3[0],
+         oc3[1],nc3[1]+oc3[1],
+         oc3[2],nc3[2]+oc3[2]);
 
   switch (precision)  {
 
   case precision_single:
 
-    return apply_(       (float *) values_f, nd3_f, im3_f, n3_f,
-			 (const float *) values_c, nd3_c, im3_c, n3_c);
+    return apply_((float *)       values_f, mf3, of3, nf3,
+		  (const float *) values_c, mc3, oc3, nc3);
 
     break;
 
   case precision_double:
 
-    return apply_(       (double *) values_f, nd3_f, im3_f, n3_f,
-			 (const double *) values_c, nd3_c, im3_c, n3_c);
+    return apply_((double *)       values_f, mf3, of3, nf3,
+		  (const double *) values_c, mc3, oc3, nc3);
 
     break;
 
@@ -62,176 +62,221 @@ int ProlongLinear::apply
 
 template <class T>
 int ProlongLinear::apply_
-(       T * values_f, int nd3_f[3], int im3_f[3], int n3_f[3],
-	const T * values_c, int nd3_c[3], int im3_c[3], int n3_c[3])
+(       T * values_f, int mf3[3], int of3[3], int nf3[3],
+	const T * values_c, int mc3[3], int oc3[3], int nc3[3])
 {
-  const int dx_c = 1;
-  const int dy_c = nd3_c[0];
-  const int dz_c = nd3_c[0]*nd3_c[1];
+  const int dcx = 1;
+  const int dcy = mc3[0];
+  const int dcz = mc3[0]*mc3[1];
 
-  int rank = (nd3_f[2] > 1) ? 3 : ( (nd3_f[1] > 1) ? 2 : 1 );
+  int rank = (mf3[1] == 1) ? 1 : ( (mf3[2] == 1) ? 2 : 3 );
 
   for (int i=0; i<rank; i++) {
     const char * xyz = "xyz";
     ASSERT3 ("ProlongLinear::apply_",
-             "fine array %c-axis %d must be 2 x coarse axis %d",
-             xyz[i],n3_c[i],n3_f[i],
-             n3_f[i]==n3_c[i]*2);
+             "fine array %c-axis %d must be 2 times coarse axis %d",
+             xyz[i],nf3[i],nc3[i],
+             nf3[i]==2*nc3[i] || nf3[i]==2*(nc3[i]-2));
 
-    ASSERT2 ("ProlongLinear::apply_",
-             "fine grid %c-axis %d must be divisible by 2",
-             xyz[i],n3_f[i],n3_f[i] % 2 == 0);
-    ASSERT2 ("ProlongLinear::apply_",
-             "fine grid %c-axis %d must be at least 4",
-             xyz[i],n3_f[i],n3_f[i] >= 4);
+    // ASSERT2 ("ProlongLinear::apply_",
+    //          "fine grid %c-axis %d must be divisible by 2",
+    //          xyz[i],nf3[i],nf3[i] % 2 == 0);
+    // ASSERT2 ("ProlongLinear::apply_",
+    //          "fine grid %c-axis %d must be at least 4",
+    //          xyz[i],nf3[i],nf3[i] >= 4);
   }
 
-  if (n3_f[1]==1) {
+  // adjustment if coarse ghost cells available
+  // NOTE:1 if ghosts not available , 0 if ghosts available
+  
+  int gcx = (nf3[0]==2*nc3[0]) ? 1 : 0;
+  int gcy = (nf3[1]==2*nc3[1]) ? 1 : 0;
+  int gcz = (nf3[2]==2*nc3[2]) ? 1 : 0;
 
-    const int ix0_f = im3_f[0];
-    const int ix0_c = im3_c[0];
-    const int nx_f = n3_f[0];
+  if (nf3[1]==1) {
 
-    for (int ix_f = 0; ix_f<nx_f; ix_f++) {
+    const int ofx = of3[0];
+    const int ocx = oc3[0];
+    const int nfx = nf3[0];
 
-      int ix_c = ((ix_f+1) >> 1) - 1;
+    for (int ifx = 0; ifx<nfx; ifx++) {
 
-      // shift coarse cells for fine grid cells on faces
-      ix_c += (ix_f==0) ? +1 : (ix_f < nx_f-1) ? 0 : -1;
+      int icx = ((ifx+1) >> 1) - gcx;
 
-      // weighting factor for even / odd odd / even
-      // take into account extrapolating at edges
-      //
-      // 1D: (5/4 -1/4) (3/4 1/4) (1/4 3/4) ... (3/4 1/4) (1/4 3/4) (-1/4 5/4)
-      //     so wy is (5/4 -1/4) or (1/4 3/4) for even,
-      //             (-1/4 5/4)  or (3/4 1/4) for odd
-      
-      int wx[2] = { (0 < ix_f && ix_f < nx_f-1) ? 1 : 5,
-		    (0 < ix_f && ix_f < nx_f-1) ? 3 : -1 };
-    
-      T wx0 = 0.25*wx[ ix_f&1];
-      T wx1 = 0.25*wx[~ix_f&1];
+      // Default weighting factor
+      int wx[2] = { 1, 3 };
 
-      int i_c = (ix0_c+ix_c) ;
-      int i_f = (ix0_f+ix_f) ;
-	  
+      // Update weights if no ghosts and on edges
+      if (ifx==0)     { icx += gcx; }
+      if (ifx==nfx-1) { icx -= gcx; }
+      if (ifx==0 || ifx==nfx-1) {
+	wx[0] += 4*gcx;
+	wx[1] -= 4*gcx;
+      }
+
+      T wx0 = 0.25*wx[ ifx&1];
+      T wx1 = 0.25*wx[~ifx&1];
+
+      int i_c = (ocx+icx) ;
+      int i_f = (ofx+ifx) ;
+
       values_f[i_f] = wx0*values_c[i_c]
-	+             wx1*values_c[i_c + dx_c ];
+	+             wx1*values_c[i_c + dcx ];
     }
 
-    return (sizeof(T) * n3_c[0]);
+    return (sizeof(T) * nc3[0]);
 
 
-  } else if (n3_f[2] == 1) {
+  } else if (nf3[2] == 1) {
 
-    const int ix0_f = im3_f[0];
-    const int iy0_f = im3_f[1];
-    const int ix0_c = im3_c[0];
-    const int iy0_c = im3_c[1];
-    const int nx_f = n3_f[0];
-    const int ny_f = n3_f[1];
-    const int mx_c = nd3_c[0];
-    const int mx_f = nd3_f[0];
+    const int ofy = of3[1];
+    const int ocy = oc3[1];
+    const int nfy = nf3[1];
     
-    for (int iy_f = 0; iy_f<ny_f; iy_f++) {
+    for (int ify = 0; ify<nfy; ify++) {
 
-      int iy_c = ((iy_f+1) >> 1) - 1;
-      iy_c += (iy_f==0) ? +1 : (iy_f < ny_f-1) ? 0 : -1;
+      int icy = ((ify+1) >> 1) - gcy;
 
-      int wy[2] = { (0 < iy_f && iy_f < ny_f-1) ? 1 : 5,
-		    (0 < iy_f && iy_f < ny_f-1) ? 3 : -1 };
-    
-      T wy0 = 0.25*wy[ iy_f&1];
-      T wy1 = 0.25*wy[~iy_f&1];
+      // Default weighting factor
+      int wy[2] = { 1, 3 };
 
-      for (int ix_f = 0; ix_f<nx_f; ix_f++) {
+      // Update weights if no ghosts and on edges
+      if (ify==0)     { icy += gcy; }
+      if (ify==nfy-1) { icy -= gcy; }
+      if (ify==0 || ify==nfy-1) {
+	wy[0] += 4*gcy;
+	wy[1] -= 4*gcy;
+      }
 
-	int ix_c = ((ix_f+1) >> 1) - 1;
-	ix_c += (ix_f==0) ? +1 : (ix_f < nx_f-1) ? 0 : -1;
+      T wy0 = 0.25*wy[ ify&1];
+      T wy1 = 0.25*wy[~ify&1];
 
-	int wx[2] = { (0 < ix_f && ix_f < nx_f-1) ? 1 : 5,
-		      (0 < ix_f && ix_f < nx_f-1) ? 3 : -1 };
+      const int ofx = of3[0];
+      const int ocx = oc3[0];
+      const int nfx = nf3[0];
 
-	T wx0 = 0.25*wx[ ix_f&1];
-	T wx1 = 0.25*wx[~ix_f&1];
+      const int mcx = mc3[0];
+      const int mfx = mf3[0];
 
-	int i_c = (ix0_c+ix_c) + mx_c * ( (iy0_c+iy_c) );
-	int i_f = (ix0_f+ix_f) + mx_f * ( (iy0_f+iy_f) );
+      for (int ifx = 0; ifx<nfx; ifx++) {
+
+	int icx = ((ifx+1) >> 1) - gcx;
+
+	// Default weighting factor
+	int wx[2] = { 1, 3 };
+
+	// Update weights if no ghosts and on edges
+	if (ifx==0)     { icx += gcx; }
+	if (ifx==nfx-1) { icx -= gcx; }
+	if (ifx==0 || ifx==nfx-1) {
+	  wx[0] += 4*gcx;
+	  wx[1] -= 4*gcx;
+	}
+
+	T wx0 = 0.25*wx[ ifx&1];
+	T wx1 = 0.25*wx[~ifx&1];
+
+	int i_c = (ocx+icx) + mcx * ( (ocy+icy) );
+	int i_f = (ofx+ifx) + mfx * ( (ofy+ify) );
 
 	values_f[i_f] = wx0*wy0*values_c[i_c]
-	  +             wx1*wy0*values_c[i_c + dx_c ]
-	  +             wx0*wy1*values_c[i_c        + dy_c ]
-	  +             wx1*wy1*values_c[i_c + dx_c + dy_c ];
+	  +             wx1*wy0*values_c[i_c + dcx ]
+	  +             wx0*wy1*values_c[i_c       + dcy ]
+	  +             wx1*wy1*values_c[i_c + dcx + dcy ];
       }
     }
 
-    return (sizeof(T) * n3_c[0]*n3_c[1]);
+    return (sizeof(T) * nc3[0]*nc3[1]);
 
   } else {
 
-    const int ix0_f = im3_f[0];
-    const int iy0_f = im3_f[1];
-    const int iz0_f = im3_f[2];
-    const int ix0_c = im3_c[0];
-    const int iy0_c = im3_c[1];
-    const int iz0_c = im3_c[2];
-    const int nx_f = n3_f[0];
-    const int ny_f = n3_f[1];
-    const int nz_f = n3_f[2];
-    const int mx_c = nd3_c[0];
-    const int mx_f = nd3_f[0];
-    const int my_c = nd3_c[1];
-    const int my_f = nd3_f[1];
+    const int ofz = of3[2];
+    const int ocz = oc3[2];
+    const int nfz = nf3[2];
+
+    for (int ifz = 0; ifz<nfz; ifz++) {
+
+      int icz = ((ifz+1) >> 1) - gcz;
+
+      // Default weighting factor
+      int wz[2] = { 1, 3 };
+
+      // Update weights if no ghosts and on edges
+      if (ifz==0)     { icz += gcz; }
+      if (ifz==nfz-1) { icz -= gcz; }
+      if (ifz==0 || ifz==nfz-1) {
+	wz[0] += 4*gcz;
+	wz[1] -= 4*gcz;
+      }
+
+      T wz0 = 0.25*wz[ ifz&1];
+      T wz1 = 0.25*wz[~ifz&1];
+
+      const int ofy = of3[1];
+      const int ocy = oc3[1];
+      const int nfy = nf3[1];
+
+      for (int ify = 0; ify<nfy; ify++) {
+
+	int icy = ((ify+1) >> 1) - gcy;
+
+	// Default weighting factor
+	int wy[2] = { 1, 3 };
+
+	// Update weights if no ghosts and on edges
+	if (ify==0)     { icy += gcy; }
+	if (ify==nfy-1) { icy -= gcy; }
+	if (ify==0 || ify==nfy-1) {
+	  wy[0] += 4*gcy;
+	  wy[1] -= 4*gcy;
+	}
+
+	T wy0 = 0.25*wy[ ify&1];
+	T wy1 = 0.25*wy[~ify&1];
+
+	const int ofx = of3[0];
+	const int ocx = oc3[0];
+	const int nfx = nf3[0];
+
+	const int mcx = mc3[0];
+	const int mfx = mf3[0];
+	const int mcy = mc3[1];
+	const int myf = mf3[1];
     
-    for (int iz_f = 0; iz_f<nz_f; iz_f++) {
+	for (int ifx = 0; ifx<nfx; ifx++) {
 
-      int iz_c = ((iz_f+1) >> 1) - 1;
-      iz_c += (iz_f==0) ? +1 : (iz_f < nz_f-1) ? 0 : -1;
+	  int icx = ((ifx+1) >> 1) - gcx;
 
-      int wz[2] = { (0 < iz_f && iz_f < nz_f-1) ? 1 : 5,
-		    (0 < iz_f && iz_f < nz_f-1) ? 3 : -1 };
-    
-      T wz0 = 0.25*wz[ iz_f&1];
-      T wz1 = 0.25*wz[~iz_f&1];
+	  // Default weighting factor
+	  int wx[2] = { 1, 3 };
 
-      for (int iy_f = 0; iy_f<ny_f; iy_f++) {
+	  // Update weights if no ghosts and on edges
+	  if (ifx==0)     { icx += gcx; }
+	  if (ifx==nfx-1) { icx -= gcx; }
+	  if (ifx==0 || ifx==nfx-1) {
+	    wx[0] += 4*gcx;
+	    wx[1] -= 4*gcx;
+	  }
 
-	int iy_c = ((iy_f+1) >> 1) - 1;
-	iy_c += (iy_f==0) ? +1 : (iy_f < ny_f-1) ? 0 : -1;
+	  T wx0 = 0.25*wx[ ifx&1];
+	  T wx1 = 0.25*wx[~ifx&1];
 
-	int wy[2] = { (0 < iy_f && iy_f < ny_f-1) ? 1 : 5,
-		      (0 < iy_f && iy_f < ny_f-1) ? 3 : -1 };
-
-	T wy0 = 0.25*wy[ iy_f&1];
-	T wy1 = 0.25*wy[~iy_f&1];
-
-	for (int ix_f = 0; ix_f<nx_f; ix_f++) {
-
-	  int ix_c = ((ix_f+1) >> 1) - 1;
-	  ix_c += (ix_f==0) ? +1 : (ix_f < nx_f-1) ? 0 : -1;
-
-	  int wx[2] = { (0 < ix_f && ix_f < nx_f-1) ? 1 : 5,
-			(0 < ix_f && ix_f < nx_f-1) ? 3 : -1 };
-
-	  T wx0 = 0.25*wx[ ix_f&1];
-	  T wx1 = 0.25*wx[~ix_f&1];
-
-	  int i_c = (ix0_c+ix_c) + mx_c*( (iy0_c+iy_c) + my_c*(iz0_c+iz_c) );
-	  int i_f = (ix0_f+ix_f) + mx_f*( (iy0_f+iy_f) + my_f*(iz0_f+iz_f) );
+	  int i_c = (ocx+icx) + mcx*( (ocy+icy) + mcy*(ocz+icz) );
+	  int i_f = (ofx+ifx) + mfx*( (ofy+ify) + myf*(ofz+ifz) );
 	  
 	  values_f[i_f] = wx0*wy0*wz0*values_c[i_c]
-	    +             wx1*wy0*wz0*values_c[i_c + dx_c ]
-	    +             wx0*wy1*wz0*values_c[i_c        + dy_c ]
-	    +             wx1*wy1*wz0*values_c[i_c + dx_c + dy_c ]
-	    +             wx0*wy0*wz1*values_c[i_c               + dz_c ]
-	    +             wx1*wy0*wz1*values_c[i_c + dx_c        + dz_c ]
-	    +             wx0*wy1*wz1*values_c[i_c        + dy_c + dz_c ]
-	    +             wx1*wy1*wz1*values_c[i_c + dx_c + dy_c + dz_c ];
+	    +             wx1*wy0*wz0*values_c[i_c + dcx ]
+	    +             wx0*wy1*wz0*values_c[i_c       + dcy ]
+	    +             wx1*wy1*wz0*values_c[i_c + dcx + dcy ]
+	    +             wx0*wy0*wz1*values_c[i_c             + dcz ]
+	    +             wx1*wy0*wz1*values_c[i_c + dcx       + dcz ]
+	    +             wx0*wy1*wz1*values_c[i_c       + dcy + dcz ]
+	    +             wx1*wy1*wz1*values_c[i_c + dcx + dcy + dcz ];
 	}
       }
     }
 
-    return (sizeof(T) * n3_c[0]*n3_c[1]*n3_c[2]);
+    return (sizeof(T) * nc3[0]*nc3[1]*nc3[2]);
 
   }
 }
