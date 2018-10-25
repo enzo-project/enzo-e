4ffc70e5dc22a52b5fb70b3bdde0e1987e026d94
aemerick11@gmail.com <aemerick11@gmail.com>
2018-10-25 09:26:47 -0400

    Bugfix: enzo_EnzoInitialGrackleTest incorrect enforce_block declaration from recent merge. Fixing this to work with updated methods.

--- src/Enzo/enzo_EnzoInitialGrackleTest.hpp
+++ src/Enzo/enzo_EnzoInitialGrackleTest.hpp
@@ -41,11 +41,7 @@ public: // interface
   /// Initialize the block
 
   virtual void enforce_block
-  (   Block * block,
-   const FieldDescr * field_descr,
-   const ParticleDescr * particle_descr,
-   const Hierarchy * hierarchy
-   ) throw();
+  (   Block * block, const Hierarchy * hierarchy ) throw();
 
   // Destructor
   virtual ~EnzoInitialGrackleTest() throw() {};
