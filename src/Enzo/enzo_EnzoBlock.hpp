858888d26474f6e9b0e28b5f87147819c67ecde7
aemerick11@gmail.com <aemerick11@gmail.com>
2018-12-17 16:56:12 -0500

    Removed unneeded commented code in enzo_EnzoBlock

--- src/Enzo/enzo_EnzoBlock.hpp
+++ src/Enzo/enzo_EnzoBlock.hpp
@@ -26,7 +26,6 @@ class EnzoBlock : public BASE_ENZO_BLOCK
   friend class EnzoTimestep;
   friend class EnzoTimestepPpm;
   friend class EnzoTimestepPpml;
-//  friend class EnzoInitialGrackleTest;
   friend class EnzoInitialImplosion2;
   friend class EnzoInitialSedovArray2;
 
