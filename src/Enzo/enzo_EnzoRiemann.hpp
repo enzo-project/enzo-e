03fcafa69b3991b71ce28c8dc289189695299eee
mabruzzo <matthewabruzzo@gmail.com>
2019-03-24 21:38:44 -0400

    forgot to include changes to enzo_EnzoRiemann.hpp in the prior commit.

--- src/Enzo/enzo_EnzoRiemann.hpp
+++ src/Enzo/enzo_EnzoRiemann.hpp
@@ -3,6 +3,7 @@
 #define ENZO_ENZO_RIEMANN_HPP
 
 #include <pup_stl.h>
+#include "enzo_EnzoRiemannFields.hpp"
 
 // This class factors out the repeated code between different approximate
 // Riemann Solvers (e.g. HLLE, HLLC, HLLD and possibly LLF & Roe solvers).
@@ -13,21 +14,18 @@
 //   refer to a helper class with a wave_speeds_ and riemann_flux_ helper
 //   function. We could then name template specializations
 //   (e.g. typedef EnzoRiemannHLLBase<HLLEHelper> EnzoRiemannHLLE)
+// The code internally uses a struct as a lookup to map array indices to
+// hydrodynamic quantities.
 //
-// The code internally uses an unordered map to keep track of the hydrodynamic
-// properties at a given cell in the mesh.
-typedef std::unordered_map<std::string,enzo_float> flt_map;
-typedef std::unordered_map<std::string,EFlt3DArray> array_map;
-// If the dynamic properties of a map slow the implementation too much, we
-// could switch to an array based system (to determine ordering of values,
-// templates can be used to calculate hash functions for strings at compile
-// time)
+// To allow for easily adding additional fields with non-trivial flux
+// calculations (e.g. cosmic rays or possibly internal energy), the
+// constructor will accept an array of functors, subclassed from FluxFunctor
+// - Again, if the functors are too slow, we could specify the functors as
+//   variadic template arguments (allowing operator() to be inlined within the
+//   for loop)
 //
-// To allow for easily adding additional fields which require non-trivial
-// flux calculations (e.g. cosmic rays or possibly internal energy),
-// the constructor will accept an array of functors, subclassed from FluxFunctor
-// Again, if the functors are too slow, we could specify the functors as
-// variadic (allowing operator() to be inlined within the for loop)
+// To add an additional fields, add the field to FLUX_TABLE and modify the
+// factory method
 //
 // Current state:
 //  - support for passive scalars is not yet implemented (though there is a
@@ -50,8 +48,9 @@ public:
     : PUP::able(m)
   {  }
 
-  virtual void operator()(const flt_map &cons, const flt_map &prim,
-			  flt_map &flux)=0;
+  virtual void operator()(const enzo_float prim[], const enzo_float cons[],
+			  enzo_float fluxes[], const field_lut prim_lut,
+			  const field_lut cons_lut)=0;
 };
 
 class EnzoRiemann : public PUP::able
@@ -66,23 +65,10 @@ public: // interface
   // The signature of this method must be modified as additional physics get's
   // added
   static EnzoRiemann* construct_riemann(std::string solver);
-					
 
-  EnzoRiemann(std::vector<std::string> &extra_scalar_groups,
-	      std::vector<std::string> &extra_vector_groups,
+  EnzoRiemann(EnzoFieldConditions cond,
 	      std::vector<std::string> &extra_passive_groups,
-	      FluxFunctor** flux_funcs, int n_funcs)
-  {
-    initializer_(extra_scalar_groups, extra_vector_groups,
-		 extra_passive_groups, flux_funcs, n_funcs);
-  }
-
-  EnzoRiemann()
-  {
-    // Empty vector
-    std::vector<std::string> temp;
-    initializer_(temp, temp, temp, NULL,0);
-  };
+	      FluxFunctor** flux_funcs, int n_funcs);
 
   /// Virtual destructor
   virtual ~EnzoRiemann();
@@ -105,56 +91,52 @@ public: // interface
 protected : //methods
 
   // Compute the Riemann Fluxes (and wave_speeds)
-  virtual void calc_riemann_fluxes_(const flt_map &flux_l,
-				    const flt_map &flux_r,
-				    const flt_map &prim_l,
-				    const flt_map &prim_r,
-				    const flt_map &cons_l,
-				    const flt_map &cons_r,
-				    std::vector<std::string> &cons_keys,
-				    std::size_t n_keys,
+  virtual void calc_riemann_fluxes_(const enzo_float flux_l[],
+				    const enzo_float flux_r[],
+				    const enzo_float prim_l[],
+				    const enzo_float prim_r[],
+				    const enzo_float cons_l[],
+				    const enzo_float cons_r[],
+				    const field_lut prim_lut,
+				    const field_lut cons_lut,
+				    const int n_keys,
 				    EnzoEquationOfState *eos,
 				    const int iz, const int iy, const int ix,
-				    array_map &flux_arrays) =0;
+				    EFlt3DArray flux_arrays[]) =0;
 
   void solve_passive_advection_(Block* block,
 				Grouping &priml_group, Grouping &primr_group,
 				EFlt3DArray &density_flux, int dim)
   { /* This needs to be implemented */ }
 
-  void combine_groups_(std::vector<std::string> &default_g,
-		       std::vector<std::string> &extra_scalar_g,
-		       std::vector<std::string> &extra_vector_g,
-		       std::vector<std::string> &combined_g);
-
-  void load_fluid_fields_(Block *block, array_map &arrays, Grouping &grouping,
-			  std::vector<std::string> &group_names, int dim,
-			  std::vector<std::string> *key_names);
-
   // computes the fast magnetosonic speed along dimension i
-  enzo_float fast_magnetosonic_speed_(const flt_map &prim_vals,
+  enzo_float fast_magnetosonic_speed_(const enzo_float prim_vals[],
+				      const field_lut prim_lut,
 				      EnzoEquationOfState *eos);
 
-  enzo_float mag_pressure_(const flt_map &prim_vals);
-  
-  enzo_float sound_speed_(const flt_map &prim_vals, EnzoEquationOfState *eos)
+  enzo_float mag_pressure_(const enzo_float prim_vals[],
+			   const field_lut prim_lut);
+
+  enzo_float sound_speed_(const enzo_float prim_vals[],
+			  const field_lut prim_lut,
+			  EnzoEquationOfState *eos)
   {
-    return std::sqrt(eos->get_gamma()*prim_vals.at("pressure")/
-		     prim_vals.at("density"));
+    return std::sqrt(eos->get_gamma()*prim_vals[prim_lut.pressure]/
+		     prim_vals[prim_lut.density]);
   }
 
-  void basic_mhd_fluxes_(const flt_map &prim, const flt_map &cons,
-			 flt_map &fluxes);
-private:
-  void initializer_(std::vector<std::string> &extra_scalar_groups,
-		    std::vector<std::string> &extra_vector_groups,
-		    std::vector<std::string> &extra_passive_groups,
-		    FluxFunctor** flux_funcs, int n_funcs);
-    
+  void basic_mhd_fluxes_(const enzo_float prim[], const enzo_float cons[],
+			 enzo_float fluxes[], const field_lut prim_lut,
+			 const field_lut cons_lut);
+
 protected: //attributes
-  std::vector<std::string> cons_groups_;
-  std::vector<std::string> prim_groups_;
-  std::size_t n_keys_;
+
+  EnzoFieldConditions conditions_;
+  field_lut cons_lut_;
+  field_lut prim_lut_;
+  int n_cons_keys_;
+  int n_prim_keys_;
+
   std::vector<std::string> passive_groups_;
   // number of flux functors
   int n_funcs_;
