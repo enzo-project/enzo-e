f2b06b7cecb4be9ce1f49be95b3b9c2c680282c2
Matthew Abruzzo <matthewabruzzo@gmail.com>
2020-05-22 18:30:22 -0400

    refactored _common_search_paths.py

--- config/_common_search_paths.py
+++ config/_common_search_paths.py
@@ -1,28 +1,26 @@
 import os
 
+def _check_dirs(search_paths):
+        for search_path in search_paths:
+                if os.path.isdir(search_path):
+                        return path
+        return None
+
 def charm_path_search(home):
 	# function that searches for charm_path in locations common across
 	# multiple architectures
 	charm_path = os.getenv('CHARM_HOME')
 
 	if (charm_path is None) and (home is not None):
-		if os.path.isdir(home + '/Charm/charm'):
-			charm_path = home + '/Charm/charm'
-		elif os.path.isdir(home + '/charm'):
-			charm_path = home + '/charm'
-		elif os.path.isdir(home + '/local/charm'):
-			charm_path = home + '/local/charm'
-		elif os.path.isdir(home + '/src/charm'):
-			charm_path = home + '/src/charm'
-		elif os.path.isdir(home + '/source/charm'):
-			charm_path = home + '/source/charm'
+                temp = ['Charm/charm', 'charm', 'local/charm', 'src/charm',
+                        'source/charm']
+                paths = [os.path.join(home, elem) for elem in temp]
+                charm_path = _check_dirs(paths)
 	if charm_path is None:
-		if os.path.isdir('/usr/local/charm'):
-			charm_path = '/usr/local/charm'
-		elif os.path.isdir('/opt/charm'):
-			charm_path = '/opt/charm'
-		else:
-			raise Exception('Charm++ was not found.	 Try setting the CHARM_HOME environment variable.')
+                charm_path = _check_dirs(['/usr/local/charm', '/opt/charm'])
+                if charm_path is None:
+			raise Exception('Charm++ was not found.	Try setting '
+                                        'the CHARM_HOME environment variable.')
 	return charm_path
 
 def grackle_path_search(home):
@@ -30,21 +28,11 @@ def grackle_path_search(home):
 	# multiple architectures
 	grackle_path = os.getenv('GRACKLE_HOME')
 	if (grackle_path is None) and (home is not None):
-		if os.path.isdir(home + '/Grackle/src/clib'):
-			grackle_path = home + '/Grackle/src/clib'
-		elif os.path.isdir(home + '/grackle/src/clib'):
-			grackle_path = home + '/grackle/src/clib'
-		elif os.path.isdir(home + '/local/grackle/src/clib'):
-			grackle_path = home + '/local/grackle/src/clib'
-		elif os.path.isdir(home + '/src/grackle/src/clib'):
-			grackle_path = home + '/src/grackle/src/clib'
-		elif os.path.isdir(home + '/source/grackle/src/clib'):
-			grackle_path = home + '/source/grackle/src/clib'
-		elif os.path.isdir(home + '/Software/Grackle/src/clib'):
-			grackle_path = home + '/Software/Grackle/src/clib'
+                temp = ['Grackle', 'grackle', 'local/grackle',
+                        'src/grackle', 'source/grackle', 'Software/Grackle']
+                paths = [os.path.join(home, elem, 'src/clib') for elem in temp]
+                grackle_path = _check_dirs(paths)
 	if grackle_path is None:
-		if os.path.isdir('/usr/local/grackle/src/clib'):
-			grackle_path = '/usr/local/grackle/src/clib'
-		elif os.path.isdir('/opt/grackle/src/clib'):
-			grackle_path = '/opt/grackle/src/clib'
+                grackle_path = _check_dirs(['/usr/local/grackle/src/clib',
+                                            '/opt/grackle/src/clib'])
 	return grackle_path
