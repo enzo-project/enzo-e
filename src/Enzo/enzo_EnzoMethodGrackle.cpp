f561300a70d127d1566d60dbf7d8a3f6f62aae04
William Hicks <whick002@login1.frontera.tacc.utexas.edu>
2021-05-20 15:49:48 -0500

    changes for multispecies hydro

--- src/Enzo/enzo_EnzoMethodGrackle.cpp
+++ src/Enzo/enzo_EnzoMethodGrackle.cpp
@@ -205,6 +205,37 @@ void EnzoMethodGrackle::initialize_grackle_chemistry_data(double current_time)
 
     EnzoPhysicsCosmology * cosmology = enzo::cosmology();
 
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //
+    //Some blocks are coming in with current_time = 0, when time doesn't start at 0 for cosmo sims
+    //This results in the expansion factor being passed in as 0, which screws up Grackle's internal units and gives "NaN in edot[1]" warning for every cell in a loop
+    if (current_time == 0) {
+       current_time = cosmology->time_from_redshift(enzo::config()->physics_cosmology_initial_redshift);
+    }
+
+
+  //put into cosmology units
+  //initialize_grackle_chemistry_data() is first called before cosmology units are applied during initial cycle
+  double current_redshift = cosmology->redshift_from_time(current_time);
+  grackle_units_.density_units  = 1.8788e-29*enzo::config()->physics_cosmology_omega_matter_now*
+      pow(enzo::config()->physics_cosmology_hubble_constant_now,2)*
+      pow(1 + current_redshift,3);
+
+  grackle_units_.length_units   = cello::Mpc_cm*enzo::config()->physics_cosmology_comoving_box_size/
+      enzo::config()->physics_cosmology_hubble_constant_now/
+      (1.0 + current_redshift);
+
+  grackle_units_.time_units     = 2.519445e17/sqrt(enzo::config()->physics_cosmology_omega_matter_now)/
+      enzo::config()->physics_cosmology_hubble_constant_now/
+      pow(1.0 + enzo::config()->physics_cosmology_initial_redshift,1.5);
+
+  grackle_units_.velocity_units = 1.22475e7*enzo::config()->physics_cosmology_comoving_box_size*
+      sqrt(enzo::config()->physics_cosmology_omega_matter_now)*
+      sqrt(1.0 + enzo::config()->physics_cosmology_initial_redshift);
+    //
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+
     cosmology->compute_expansion_factor(&cosmo_a, &cosmo_dt,
                                         current_time);
     grackle_units_.a_units
@@ -264,6 +295,31 @@ void EnzoMethodGrackle::setup_grackle_units (EnzoBlock * enzo_block,
       Field field = enzo_block->data()->field();
       compute_time = field.history_time(i_hist);
     }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    if (compute_time == 0) {
+       //std::cout << "EnzoMethodGrackle::setup_grackle_units(): compute_time = 0 for some reason when it shouldn't be, changing to initial_time_in_code_units()" << std::endl;
+       compute_time = cosmology->time_from_redshift(enzo::config()->physics_cosmology_initial_redshift);
+       }
+       //put into cosmology units
+       //initialize_grackle_chemistry_data() is first called before cosmology units are applied
+       double current_redshift = cosmology->redshift_from_time(compute_time);
+       grackle_units->density_units  = 1.8788e-29*enzo::config()->physics_cosmology_omega_matter_now*
+           pow(enzo::config()->physics_cosmology_hubble_constant_now,2)*
+           pow(1 + current_redshift,3);
+
+       grackle_units->length_units   = cello::Mpc_cm*enzo::config()->physics_cosmology_comoving_box_size/
+           enzo::config()->physics_cosmology_hubble_constant_now/
+           (1.0 + current_redshift);
+
+       grackle_units->time_units     = 2.519445e17/sqrt(enzo::config()->physics_cosmology_omega_matter_now)/
+           enzo::config()->physics_cosmology_hubble_constant_now/
+           pow(1.0 + enzo::config()->physics_cosmology_initial_redshift,1.5);
+
+       grackle_units->velocity_units = 1.22475e7*enzo::config()->physics_cosmology_comoving_box_size*
+           sqrt(enzo::config()->physics_cosmology_omega_matter_now)*
+           sqrt(1.0 + enzo::config()->physics_cosmology_initial_redshift);
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     cosmology->compute_expansion_factor(&cosmo_a, &cosmo_dt,
                                         compute_time);
