3df4172523a130954b641a557c49cf34316ef58f
Stefan Arridge <stefan.arridge@gmail.com>
2021-06-10 21:03:18 +0100

    Fixed bug in ParticleDescr is_attribute and is_constant methods

--- src/Cello/data_ParticleDescr.cpp
+++ src/Cello/data_ParticleDescr.cpp
@@ -256,9 +256,8 @@ int ParticleDescr::attribute_index (int it, std::string attribute_name) const
 bool ParticleDescr::is_attribute(int it, std::string attribute_name) const
 {
   ASSERT1("ParticleDescr::is_attribute",
-          "Trying to access unknown particle type '%s'",
-          type_name(it).c_str(),
-	  (0 <= it && it < num_types()));
+          "Trying to access unknown particle type %d",
+          it, (0 <= it && it < num_types()));
     
   auto iter=attribute_index_[it].find(attribute_name);
 
@@ -413,9 +412,8 @@ int ParticleDescr::constant_offset (int it, int ic) const
 bool ParticleDescr::is_constant(int it, std::string constant_name) const
 {
   ASSERT1("ParticleDescr::constant_index",
-          "Trying to access unknown particle type '%s'",
-          type_name(it).c_str(),
-	  (0 <= it && it < num_types()));
+          "Trying to access unknown particle type %d",
+          it, (0 <= it && it < num_types()));
 
   auto iter=constant_index_[it].find(constant_name);
 
