d9fb574e62878561f1039fe25db3fd9f0eb7239d
mabruzzo <matthewabruzzo@gmail.com>
2021-06-10 13:02:35 -0400

    Introduced the store_fluxes_for_corrections_ attribute to both EnzoMethodMHDVlct and EnzoMethodPpm (whose value indicates whether fluxes need to be saved for flux corrections).
    Without this change, unigrid simulations would have a memory leak (i.e. EnzoMethodMHDVlct and EnzoMethodPpm would allocate buffers for storing the boundary fluxes during every cycle, but the buffers would never be deallocated).

--- src/Enzo/enzo_EnzoMethodPpm.cpp
+++ src/Enzo/enzo_EnzoMethodPpm.cpp
@@ -32,9 +32,10 @@
 
 //----------------------------------------------------------------------
 
-EnzoMethodPpm::EnzoMethodPpm ()
+EnzoMethodPpm::EnzoMethodPpm (bool store_fluxes_for_corrections)
   : Method(),
-    comoving_coordinates_(enzo::config()->physics_cosmology)
+    comoving_coordinates_(enzo::config()->physics_cosmology),
+    store_fluxes_for_corrections_(store_fluxes_for_corrections)
 {
 
   const int rank = cello::rank();
@@ -82,6 +83,7 @@ void EnzoMethodPpm::pup (PUP::er &p)
   Method::pup(p);
 
   p | comoving_coordinates_;
+  p | store_fluxes_for_corrections_;
 }
 
 //----------------------------------------------------------------------
@@ -104,19 +106,21 @@ void EnzoMethodPpm::compute ( Block * block) throw()
   if (rank >= 3) COPY_FIELD(block,"acceleration_z","acceleration_z_in");
 #endif
 
-  Field field = block->data()->field();
+  if (store_fluxes_for_corrections_){
+    Field field = block->data()->field();
 
-  auto field_names = field.groups()->group_list("conserved");
-  const int nf = field_names.size();
-  std::vector<int> field_list;
-  field_list.resize(nf);
-  for (int i=0; i<nf; i++) {
-    field_list[i] = field.field_id(field_names[i]);
+    auto field_names = field.groups()->group_list("conserved");
+    const int nf = field_names.size();
+    std::vector<int> field_list;
+    field_list.resize(nf);
+    for (int i=0; i<nf; i++) {
+      field_list[i] = field.field_id(field_names[i]);
+    }
+
+    int nx,ny,nz;
+    field.size(&nx,&ny,&nz);
+    block->data()->flux_data()->allocate (nx,ny,nz,field_list);
   }
-
-  int nx,ny,nz;
-  field.size(&nx,&ny,&nz);
-  block->data()->flux_data()->allocate (nx,ny,nz,field_list);
 
   if (block->is_leaf()) {
 
