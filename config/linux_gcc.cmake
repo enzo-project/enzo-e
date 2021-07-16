5190f0ec33ecb46c6e8650523e8c3bf434016481
Philipp Grete <grete@pa.msu.edu>
2021-07-16 06:17:27 -0400

    Add existing configurable options

--- config/linux_gcc.cmake
+++ config/linux_gcc.cmake
@@ -1,6 +1,25 @@
-message(STATUS "Loading machine configuration for generic Linux machine with GCC stack.\n")
+# Machine configs are included twice.
+# First to set compilers, paths, default options.
+# Second to set dependent options (e.g., also depending on defaults set in the main CMakeLists.txt)
+if(NOT __processedUserDefaults)
 
-set(CMAKE_CXX_COMPILER g++ CACHE STRING "")
-set(CMAKE_C_COMPILER gcc CACHE STRING "")
-set(CMAKE_Fortran_COMPILER gfortran CACHE STRING "")
-set(CMAKE_Fortran_FLAGS "-ffixed-line-length-132 -fdefault-real-8 -fdefault-double-8" CACHE STRING "Default Fortran flags")
+  message(STATUS "Loading machine configuration for generic Linux machine with GCC stack.\n")
+
+  # Setting compilers
+  set(CMAKE_CXX_COMPILER g++ CACHE STRING "")
+  set(CMAKE_C_COMPILER gcc CACHE STRING "")
+  set(CMAKE_Fortran_COMPILER gfortran CACHE STRING "")
+  set(CMAKE_Fortran_FLAGS "-ffixed-line-length-132" CACHE STRING "Default Fortran flags")
+
+  # Setting package paths (e.g., Grackle)
+
+  # Mark done
+  set(__processedUserDefaults ON)
+
+else()
+
+  if (USE_DOUBLE_PREC)
+    string(APPEND CMAKE_Fortran_FLAGS " -fdefault-real-8 -fdefault-double-8")
+  endif()
+
+endif()
--- config/pleiades_icc.cmake
+++ config/pleiades_icc.cmake
@@ -1,11 +1,28 @@
-message(STATUS "Loading machine configuration for NASA Pleiades supercomputer.\n"
-  "This configuration has been tested using the following modules:\n"
-  "$ module use -a /nasa/modulefiles/testing\n"
-  "$ module purge\n"
-  "$ module load pkgsrc/2021Q1 gcc/10.2 mpi-hpe/mpt.2.23 boost/1.75 comp-intel/2020.4.304 hdf5/1.12.0_serial pkgsrc/2021Q1\n")
+# Machine configs are included twice.
+# First to set compilers, paths, default options.
+# Second to set dependent options (e.g., also depending on defaults set in the main CMakeLists.txt)
+if(NOT __processedUserDefaults)
 
+  message(STATUS "Loading machine configuration for NASA Pleiades supercomputer.\n"
+    "This configuration has been tested using the following modules:\n"
+    "$ module use -a /nasa/modulefiles/testing\n"
+    "$ module purge\n"
+    "$ module load pkgsrc/2021Q1 gcc/10.2 mpi-hpe/mpt.2.23 boost/1.75 comp-intel/2020.4.304 hdf5/1.12.0_serial pkgsrc/2021Q1\n")
 
-set(CMAKE_CXX_COMPILER icpc CACHE STRING "")
-set(CMAKE_C_COMPILER icc CACHE STRING "")
-set(CMAKE_Fortran_COMPILER ifort CACHE STRING "")
-set(CMAKE_Fortran_FLAGS "-real-size 64 -double-size 64 -nofor-main" CACHE STRING "Default Fortran flags")
+  set(CMAKE_CXX_COMPILER icpc CACHE STRING "")
+  set(CMAKE_C_COMPILER icc CACHE STRING "")
+  set(CMAKE_Fortran_COMPILER ifort CACHE STRING "")
+  set(CMAKE_Fortran_FLAGS "-nofor-main" CACHE STRING "Default Fortran flags")
+
+  # Setting package paths (e.g., Grackle)
+
+  # Mark done
+  set(__processedUserDefaults ON)
+
+else()
+
+  if (USE_DOUBLE_PREC)
+    string(APPEND CMAKE_Fortran_FLAGS " -real-size 64 -double-size 64")
+  endif()
+
+endif()
