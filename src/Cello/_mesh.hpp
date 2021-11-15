b727b3c9cfc9d22370c582cc9c169b525fd739eb
mabruzzo <matthewabruzzo@gmail.com>
2021-11-15 11:39:05 -0500

    Excised the the Tree, Node, NodeTrace, and ItNode classes from the mesh layer (they were unused).
    I also had to remove tests for these classes

--- src/Cello/_mesh.hpp
+++ src/Cello/_mesh.hpp
@@ -102,19 +102,11 @@ enum adapt_type {
 
 // Hierarchy and components
 
-class Tree;
-
 #include "mesh_Index.hpp"
 
 #include "mesh_Block.hpp"
 #include "mesh_Hierarchy.hpp"
 #include "mesh_Factory.hpp"
-
-// Tree and components (not used in Cello)
-#include "mesh_Node.hpp"
-#include "mesh_NodeTrace.hpp"
-#include "mesh_Tree.hpp"
-#include "mesh_ItNode.hpp"
 
 // Iterators
 #include "mesh_ItFace.hpp"
