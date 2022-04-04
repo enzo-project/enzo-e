72ab60a984f72eab8ef3dc6046f5549a0eb25d01
Stefan Arridge <stefan.arridge@gmail.com>
2022-04-04 15:38:58 +0100

    Fixed bug in EnzoMethodAccretionRemoveGas::compute which meant that invalid indices where included in the loop in which density_accreted and density_accreted_accumulate were subtracted from density. Now the loop is only over active cells.

--- src/Enzo/enzo_EnzoMethodAccretionRemoveGas.cpp
+++ src/Enzo/enzo_EnzoMethodAccretionRemoveGas.cpp
@@ -16,10 +16,9 @@ EnzoMethodAccretionRemoveGas::EnzoMethodAccretionRemoveGas()
 {
 
   // Refresh operation accumulates values of density_accreted from neighbouring
-  // blocks into density_accreted accumulate. 
+  // blocks into density_accreted_accumulate. 
   cello::simulation()->refresh_set_name(ir_post_,name());
   Refresh * refresh = cello::refresh(ir_post_);
-  ParticleDescr * particle_descr = cello::particle_descr();
   refresh->set_accumulate(true);
   refresh->add_field_src_dst("density_accreted","density_accreted_accumulate");
 }
@@ -72,42 +71,35 @@ void EnzoMethodAccretionRemoveGas::compute_(Block * block)
   const int ida  = field.field_id("density_accreted");
   const int idaa = field.field_id("density_accreted_accumulate");
 
-  int nx,ny,nz;
   int gx,gy,gz;
-  field.dimensions (0,&nx,&ny,&nz);
+  int mx,my,mz;
+  field.dimensions (0,&mx,&my,&mz);
   field.ghost_depth(0,&gx,&gy,&gz);
-
-  int mx = nx + 2*gx;
-  int my = ny + 2*gy;
-  int mz = nz + 2*gz;
-
-  int m = mx * my * mz;
 
   enzo_float * density = (enzo_float*) field.values(id);
   enzo_float * density_accreted = (enzo_float*) field.values(ida);
   enzo_float * density_accreted_accumulate = (enzo_float*) field.values(idaa);
 
-  // Iterate over active cells. density_accreted and density_accreted_accumulate
-  // have negative values, and are added to density to remove gas which has been
-  // accreted onto a star particle
-
-  for (int iz=gz; iz<nz+gz; iz++){
-    for (int iy=gy; iy<ny+gy; iy++){
-      for (int ix=gx; ix<nx+gx; ix++){
-
+  // Iterate over active cells, and subtract both density_accreted and
+  // density_accreted_accumulate from density
+  for (int iz=gz; iz<mz-gz; iz++){
+    for (int iy=gy; iy<my-gy; iy++){
+      for (int ix=gx; ix<mx-gx; ix++){
+      
 	int i = INDEX(ix,iy,iz,mx,my);
-	density[i] += density_accreted[i];
-	density[i] += density_accreted_accumulate[i];
+	density[i] -= density_accreted[i];
+	density[i] -= density_accreted_accumulate[i];
       }
     }
   }
-	 
-  // Now set density_accreted and density_accreted_accumulate to zero in all cells,
-  // including in ghost zones
 
-  for (int i = 0; i < m; i++){
+  // Now set density_accreted and density_accreted_accumulate to zero
+  // in all cells.
+
+  for (int i = 0; i < mx * my * mz; i++){
     density_accreted[i] = 0.0;
     density_accreted_accumulate[i] = 0.0;
   }
+
   return;
 }
