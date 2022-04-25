cdd93745b98e7b816630b5e5ec0d9a7e2b17c3d0
James Bordner <jobordner@ucsd.edu>
2022-04-25 15:57:27 -0700

    Adding plantuml diagram to design-input.rst

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,9 +26,11 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.intersphinx',
-              'sphinx.ext.todo',]
+              'sphinx.ext.todo',
+              'sphinxcontrib.plantuml',]
 todo_include_todos=True
 
+plantuml = '/usr/bin/plantuml'
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
