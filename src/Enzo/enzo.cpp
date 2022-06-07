3511517000b3154cb07a540d7798648edf138ed2
Stefan Arridge <stefan.arridge@gmail.com>
2022-06-07 13:11:32 +0100

    Restored src/Enzo/enzo.cpp to main branch version.

--- src/Enzo/enzo.cpp
+++ src/Enzo/enzo.cpp
@@ -2,31 +2,45 @@
 
 namespace enzo {
 
-EnzoSimulation *simulation() { return proxy_enzo_simulation.ckLocalBranch(); }
-
-EnzoProblem *problem() { return (EnzoProblem *)simulation()->problem(); }
-
-const EnzoConfig *config() {
-  return (const EnzoConfig *)simulation()->config();
-}
-
-EnzoPhysicsCosmology *cosmology() {
-  return (EnzoPhysicsCosmology *)problem()->physics("cosmology");
-}
-
-const EnzoMethodGrackle *grackle_method() {
-  if (!enzo::config()->method_grackle_use_grackle) {
-    return NULL;
+  EnzoSimulation * simulation()
+  {
+    return proxy_enzo_simulation.ckLocalBranch();
   }
-  return (const EnzoMethodGrackle *)problem()->method("grackle");
+
+  EnzoProblem * problem()
+  {
+    return (EnzoProblem *) simulation()->problem();
+  }
+
+  const EnzoConfig * config()
+  {
+    return (const EnzoConfig *) simulation()->config();
+  }
+
+  EnzoPhysicsCosmology * cosmology()
+  {
+    return (EnzoPhysicsCosmology *) problem()->physics("cosmology");
+  }
+
+  const EnzoMethodGrackle * grackle_method()
+  {
+    if (!enzo::config()->method_grackle_use_grackle) {return NULL;}
+    return (const EnzoMethodGrackle *) problem()->method("grackle");
+  }
+
+  EnzoUnits * units()
+  {
+    return (EnzoUnits *) problem()->units();
+  }
+
+  CProxy_EnzoBlock block_array()
+  {
+    return (CProxy_EnzoBlock) enzo::simulation()->hierarchy()->block_array();
+  }
+
+  EnzoBlock * block ( Block * block)
+  {
+    return static_cast<EnzoBlock*> (block);
+  }
+
 }
-
-EnzoUnits *units() { return (EnzoUnits *)problem()->units(); }
-
-CProxy_EnzoBlock block_array() {
-  return (CProxy_EnzoBlock)enzo::simulation()->hierarchy()->block_array();
-}
-
-EnzoBlock *block(Block *block) { return static_cast<EnzoBlock *>(block); }
-
-} // namespace enzo
