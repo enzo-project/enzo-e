30d49fd6c24aaa54e84a5d4309214603b4747811
Matthew Abruzzo <matthewabruzzo@gmail.com>
2022-06-16 16:27:44 +0100

    Updated the Inclined-Linear-Jeans test to fix the test. Also made the test run the problem with the PPM solver.

--- input/Gravity/run_stable_jeans_wave_test.py
+++ input/Gravity/run_stable_jeans_wave_test.py
@@ -1,16 +1,15 @@
 #!/bin/python
 
-# runs VLCT HD inclined stable Jeans Wave Tests
+# runs VLCT HD and PPM HD inclined stable Jeans Wave Tests
 # - This should probably be transitioned to testing framework once it's created
 # - This script expects to be called from the root level of the repository
 #   OR at the same level where its defined
 #
 # These tests draw loose inspiration from Linear Wave tests used by Athena++.
-# Specifically, this script:
-#   1.) Checks if that L1-norm of the error is appropriate for the inclined
-#       stable jeans wave for N=16 and N=32
+# Specifically, this script: Checks if that L1-norm of the error is appropriate
+# for the inclined stable jeans wave for N=16 and N=32
 #
-# In the future, this should also be extended to test the PPM solver
+# In the future, we may want to split the PPM and VL+CT tests
 
 import argparse
 import os.path
@@ -36,31 +35,43 @@ else:
                        "but that that directory does not exist")
 
 def run_tests(executable):
-    temp = 'input/Gravity/jeans_wave/vlct_stable_jeansN{:d}.in'
-    call_test = EnzoEWrapper(executable,temp)
 
-    call_test(16)
-    call_test(32)
+    for solver in ['ppm', 'vlct']:
+
+        temp = 'input/Gravity/jeans_wave/' + solver + '_stable_jeansN{:d}.in'
+        call_test = EnzoEWrapper(executable,temp)
+
+        call_test(16)
+        call_test(32)
 
 def analyze_tests():
     # define the functor for evaluating the norm of the L1 error vector
     l1_func = CalcSimL1Norm(["density","velocity_x","velocity_y","velocity_z",
                              "pressure",])
-    template = "method_vlct-cg-1-inclined-jeansN{res}_{time:.4f}"
 
     verbose = False
-    err_compare = partial(analyze_linwave, target_template = template,
-                          name_template = 'stable-jeans wave N={res}',
-                          l1_functor = l1_func,
-                          final_times = {'stable-jeans' : 1.0},
-                          verbose = verbose)
+    err_cmp_vlct = partial(
+        analyze_linwave,
+        target_template = "method_vlct-cg-1-inclined-jeansN{res}_{time:.4f}",
+        name_template = 'vlct-stable-jeans wave N={res}', l1_functor = l1_func,
+        final_times = {'vlct-stable-jeans' : 1.1547}, verbose = verbose
+    )
+    err_cmp_ppm = partial(
+        analyze_linwave,
+        target_template = "method_ppm-cg-1-inclined-jeansN{res}_{time:.4f}",
+        name_template = 'ppm-stable-jeans wave N={res}', l1_functor = l1_func,
+        final_times = {'ppm-stable-jeans' : 1.1547}, verbose = verbose
+    )
+
     r = []
     # first let's run the l1-norm of each value for 2 sizes to make sure we get
     # convergence (originally we were looking at the slope of convergence but
     # that allows the errors to be larger)
 
-    r.append(err_compare(1.2316886043695045e-07, 'stable-jeans', 1,16))
-    r.append(err_compare(1.903655646665732e-08, 'stable-jeans', 1,32))
+    r.append(err_cmp_vlct(5.104682632606632e-07, 'vlct-stable-jeans', 1,16))
+    r.append(err_cmp_vlct(2.2411686465108514e-07, 'vlct-stable-jeans', 1,32))
+    r.append(err_cmp_ppm(1.6245498643250925e-07, 'ppm-stable-jeans', 1,16))
+    r.append(err_cmp_ppm(7.379045545039628e-08, 'ppm-stable-jeans', 1,32))
 
     n_passed = np.sum(r)
     n_tests = len(r)
@@ -73,7 +84,11 @@ def cleanup():
     dir_names = ["method_vlct-cg-1-inclined-jeansN16_0.0000",
                  "method_vlct-cg-1-inclined-jeansN16_1.0000",
                  "method_vlct-cg-1-inclined-jeansN32_0.0000",
-                 "method_vlct-cg-1-inclined-jeansN32_1.0000"]
+                 "method_vlct-cg-1-inclined-jeansN32_1.0000",
+                 "method_ppm-cg-1-inclined-jeansN16_0.0000",
+                 "method_ppm-cg-1-inclined-jeansN16_1.0000",
+                 "method_ppm-cg-1-inclined-jeansN32_0.0000",
+                 "method_ppm-cg-1-inclined-jeansN32_1.0000"]
 
     for dir_name in dir_names:
         if os.path.isdir(dir_name):
