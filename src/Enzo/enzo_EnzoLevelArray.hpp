ed00103b2af6b04aeede1efdd3e03f0ba8f0488c
James Bordner <jobordner@ucsd.edu>
2023-05-22 17:10:30 -0700

    Fixed indexing bugs in EnzoMethodInference
      EnzoMethodInference
      - [ ] renamed i -> i_f for index of field in vector to avoid name clashes
      - [ ] fixed coarsen_() parameters
      - [ ] fixed copy_() parameters
      - [ ] removed unused copy_() parameters
      - [ ] removed dead code

--- src/Enzo/enzo_EnzoLevelArray.hpp
+++ src/Enzo/enzo_EnzoLevelArray.hpp
@@ -80,10 +80,8 @@ protected: // functions
    int mcx, int mcy, int mcz, int ncx, int ncy, int ncz, int ecx, int ecy, int ecz);
 
   void copy_
-  (enzo_float * af,
-   int mfx, int mfy, int mfz, int nfx, int nfy, int nfz, int efx, int efy, int efz,
-   const enzo_float * ac,
-   int mcx, int mcy, int mcz, int ncx, int ncy, int ncz, int ecx, int ecy, int ecz);
+  (      enzo_float * af, int mfx, int mfy, int mfz, int nfx, int nfy, int nfz,
+   const enzo_float * ac, int mcx, int mcy, int mcz, int ncx, int ncy, int ncz);
 
 private: // attributes
 
