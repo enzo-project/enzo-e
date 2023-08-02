f2fe1953a2279c6218c9e082f10fccc682c9ccce
Matthew Abruzzo <matthewabruzzo@gmail.com>
2023-08-02 18:11:04 -0400

    update SetMinimumSupport to use a consistent gravitational constant

--- src/Enzo/hydro-mhd/SetMinimumSupport.cpp
+++ src/Enzo/hydro-mhd/SetMinimumSupport.cpp
@@ -48,11 +48,29 @@ int EnzoBlock::SetMinimumSupport(enzo_float &MinimumSupportEnergyCoefficient,
     enzo_float * velocity_y      = (enzo_float*) field.values("velocity_y");
     enzo_float * velocity_z      = (enzo_float*) field.values("velocity_z");
 
-    /* Set minimum GE. */
+    // Set minimum specific internal energy (aka gas energy)
+    //
+    // minimum pressure support sets the specific thermal energy such that
+    //   lambda_J >= sqrt(K) * CellWidth
+    // where lambda_J is the Jeans length or c_s * sqrt(pi / (G * rho)) and K
+    // is MinimumPressureSupportParameter
+    //
+    // We can manipulate this inequality:
+    //   c_s * sqrt(pi / (G * rho)) >= sqrt(K) * CellWidth
+    //   c_s^2 *  pi / (G * rho) >= K * CellWidth^2
+    //   gamma * (gamma - 1) * eint * pi / (G * rho) >= K * CellWidth^2
+    //   eint >= G * K * CellWidth^2 * rho / (pi * gamma * (gamma - 1))
+    //   eint >= MinimumSupportEnergyCoefficient * rho
+    //
+    // I'm don't totally understand where the extra CosmoFactor comes in...
+    // that was here earlier
+
+    // TODO: check that CellWidth[0], CellWidth[1], and CellWidth[2] are
+    //       identical
 
     const enzo_float gamma = enzo::fluid_props()->gamma();
     MinimumSupportEnergyCoefficient =
-      GravitationalConstant[in]/(4.0*cello::pi) / (cello::pi * (gamma*(gamma-1.0))) *
+      enzo::grav_constant_codeU() / (cello::pi * (gamma*(gamma-1.0))) *
       CosmoFactor * MinimumPressureSupportParameter[in] *
       CellWidth[0] * CellWidth[0];
 
