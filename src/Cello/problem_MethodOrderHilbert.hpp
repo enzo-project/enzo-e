427e5f78abf5696e0e19e47f927bd667cfbdc525
brenjohn <johnmarkbrennan@gmail.com>
2024-02-09 14:37:19 +0000

    Add Hilbert order option to EnzoMethodBalance

--- src/Cello/problem_MethodOrderHilbert.hpp
+++ src/Cello/problem_MethodOrderHilbert.hpp
@@ -80,6 +80,18 @@ private: // methods
   /// Return the pointer to the Block's weight (including self)
   Sync * psync_weight_(Block * block);
 
+  /// Write child blocks of the given block to the children array in the hilbert order.
+  void hilbert_children(Block * block, int* children);
+
+  /// Return the hilbert state for the next recursive layer (ie for the child blocks of the given index)
+  int next_hilbert_state(Index index);
+
+  /// Return the index appearing after the given index in the hilber order.
+  Index hilbert_next (Index index, int rank, bool is_leaf, int min_level);
+
+  /// Write, to the given states array, the recursive states of the given index up to level m. 
+  void hilbert_states(Index index, int m, int* states);
+
 private: // functions
 
 
@@ -111,11 +123,6 @@ private: // attributes
   static int HNM[12][8];
   static int PHM[12][8];
   static int PNM[12][8];
-
-  void hilbert_children(Block * block, int* children);
-  int next_hilbert_state(Index index);
-  Index hilbert_next (Index index, int rank, bool is_leaf, int min_level);
-  void hilbert_states(Index index, int m, int* states);
 };
 
 #endif /* PROBLEM_METHOD_ORDER_HILBERT_HPP */
