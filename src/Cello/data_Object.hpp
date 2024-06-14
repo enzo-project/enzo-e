315791c30a65a5f4aa302f258f090044db1948da
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-06-14 10:53:40 -0400

    minor bugfix - Object's copy-assignment operation is currently wrong. It doesn't return anything.

--- src/Cello/data_Object.hpp
+++ src/Cello/data_Object.hpp
@@ -17,8 +17,7 @@ class Object : public PUP::able {
 public: // interface
 
   /// Constructor
-  Object() throw()
-  { }
+  Object() = default;
 
   /// Charm++ PUP::able declarations
   PUPable_abstract(Object);
@@ -28,12 +27,10 @@ public: // interface
   { }
 
   /// Copy constructor
-  Object(const Object & Object) throw()
-  { }
+  Object(const Object & Object) = default;
 
-  /// Assignment operator
-  Object & operator= (const Object & Object) throw()
-  { }
+  /// Copy assignment operator
+  Object & operator= (const Object & Object) = default;
 
   /// Destructor
   virtual ~Object() throw()
