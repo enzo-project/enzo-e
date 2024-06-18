b337b3e399d76ec79a09964c5b1ab4147df45052
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-06-18 11:26:30 -0400

    Implement automatic installation of Grackle

--- dependencies.cmake
+++ dependencies.cmake
@@ -8,26 +8,76 @@ find_package(Charm REQUIRED)
 STRING(REGEX REPLACE "<CMAKE_CXX_COMPILER>" "${CHARM_LINKER}"
        CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE}")
 
+# ---------------------------------------------------------------------------
+# Next, handle all dependencies that may be compiled as part of this build
+# ---------------------------------------------------------------------------
+# If we want to build these dependencies, there are 2 ways to do this:
+# 1. assume that the source code is embedded within the GitHub repository in
+#    a known location (this is usually accomplished via git submodules)
+# 2. automatically download the source-code of the dependency with cmake's
+#    FetchContent functionality
+#
+# For now, we adopt the FetchContent functionality since the cmake devs view
+# that as the "canonical" way to do things
+# -> once we can require that everyone uses CMake 3.24.2 or newer, we can
+#    leverage some useful features built into FetchContent that will make
+#    swapping between find_package and FetchContent much more seamless
+#
+# NOTE: when using FetchContent with git repositories, it is considered
+# idiomatic to use commit-hashes rather than tag-names or commit names.
+# -> the reason is that hashes can't change (while the other 2 choices can)
+# -> if we don't use a hash, then cmake will contact the remote repository
+#    between every build to check whether the tag or branch name
+
+include(FetchContent)
 
 # Need to process Grackle here as both Cello and Enzo-E depend on it
 option(USE_GRACKLE "Use Grackle Chemistry" ON)
-# don't bother advertising the following option (but users can
-# overwrite it if they really want to - e.g. to reduce binary size)
-option(GRACKLE_USE_STATIC_LIBS  "sets Grackle's lib-type if USE_GRACKLE=ON" ON)
-if (USE_GRACKLE)
-  find_package(Grackle)
-  if (Grackle_FOUND)
-    # This really only needs to be defined for a relatively small subset of
-    # files in the Enzo-layer
-    add_compile_definitions(CONFIG_USE_GRACKLE)
+option(USE_EXTERNAL_GRACKLE "Use external grackle. Meaningless unless USE_GRACKLE is ON" OFF)
+if(USE_GRACKLE)
+  # first, declare where to find FetchContent would find grackle (even if we
+  # don't ultimately use FetchContent
 
+  # TODO: shift this to mainline Grackle Repository after
+  #         https://github.com/grackle-project/grackle/pull/182
+  #       get's merged into Grackle
+  FetchContent_Declare(Grackle
+    GIT_REPOSITORY https://github.com/mabruzzo/grackle
+    GIT_TAG 689be185ac55dba098309e2da9d6acdda37d1923
+    # ^ current hash is right after cmake build-system got introduced
+  )
+
+  # some time after the following PR is merged
+  #    https://github.com/grackle-project/grackle/pull/204
+  # And we feel comfortable requiring that people use a CMake-build of grackle
+  # that ships a GrackleConfig.cmake file, a better default behavior might be
+  # to always search for Grackle first and then perform an in-source build if
+  # it can't be found
+
+  if (USE_EXTERNAL_GRACKLE)
+    find_package(Grackle)
+
+    if (NOT Grackle_FOUND)
+      message(FATAL_ERROR
+        "Configured to use Grackle but Grackle library not found.\n"
+        "Either disable grackle (e.g. `-DUSE_GRACKLE=OFF`), start using "
+        "automatic dependency management (e.g. `-DUSE_EXTERNAL_GRACKLE=OFF), "
+        "or provide path "
+        "(e.g. `-DGrackle_ROOT=/PATH/TO/GRACKLE/INSTALL/DIRECTORY`).")
+    endif()
   else()
-    message(FATAL_ERROR
-      "Configured to use Grackle but Grackle library not found.\n"
-      "Either disable grackle (e.g., `-DUSE_GRACKLE=OFF`) or provide path "
-      "(e.g., `-DGrackle_ROOT=/PATH/TO/GRACKLE/INSTALL/DIRECTORY`).")
+    set(GRACKLE_USE_DOUBLE USE_DOUBLE_PREC)
+    set(GRACKLE_USE_OPENMP OFF)
+
+    message("downloading Grackle...")
+    FetchContent_MakeAvailable(Grackle)
   endif()
+
+  # This really only needs to be defined for a relatively small subset of
+  # files in the Enzo-layer
+  add_compile_definitions(CONFIG_USE_GRACKLE)
 endif()
+
 
 find_package(PNG REQUIRED)
 add_compile_definitions(NO_FREETYPE)
